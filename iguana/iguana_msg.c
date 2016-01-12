f973caf0c49f4dc228bcb60714edbdd7a3bdd8ff
jl777 <jameslee777@yahoo.com>
2016-01-12 19:15:22 -0300

    test

--- iguana/iguana_msg.c
+++ iguana/iguana_msg.c
@@ -438,7 +438,7 @@ int32_t iguana_msgparser(struct iguana_info *coin,struct iguana_peer *addr,struc
         strcpy(addr->lastcommand,H->command);
     }
     retval = 0;
-    //printf("iguana_msgparser %s parse.(%s)\n",addr->ipaddr,H->command);
+    printf("iguana_msgparser %s parse.(%s)\n",addr->ipaddr,H->command);
     if ( strcmp(H->command,"SuperNET") == 0 )
     {
         addr->supernet = 1;
