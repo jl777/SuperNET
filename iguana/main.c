3c1d3668b617dbd52186c7bc7795828afd97dcd8
jl777 <jameslee777@yahoo.com>
2016-01-12 20:59:06 -0300

    test

--- iguana/main.c
+++ iguana/main.c
@@ -35,7 +35,7 @@ uint64_t IGUANA_MY64BITS;
 queue_t helperQ,jsonQ,finishedQ;
 static int32_t initflag;
 #ifdef __linux__
-int32_t IGUANA_NUMHELPERS = 4;
+int32_t IGUANA_NUMHELPERS = 8;
 #else
 int32_t IGUANA_NUMHELPERS = 1;
 #endif
