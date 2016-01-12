58ea40bfbb250feffea7576d26f20dff426f33c2
jl777 <jameslee777@yahoo.com>
2016-01-12 18:19:55 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -580,7 +580,7 @@ int32_t iguana_needhdrs(struct iguana_info *coin)
 int32_t iguana_reqhdrs(struct iguana_info *coin)
 {
     int32_t i,lag,n = 0; struct iguana_bundle *bp; char hashstr[65];
-    if ( iguana_needhdrs(coin) > 0 && queue_size(&coin->hdrsQ) == 0 )
+    if ( iguana_needhdrs(coin) > 0 )//&& queue_size(&coin->hdrsQ) == 0 )
     {
         //if ( coin->zcount++ > 1 )
         {
