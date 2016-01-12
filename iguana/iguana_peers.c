e0eab639dcb77f986aa4436de1dcdfdeaa137802
jl777 <jameslee777@yahoo.com>
2016-01-12 12:43:15 -0300

    test

--- iguana/iguana_peers.c
+++ iguana/iguana_peers.c
@@ -472,7 +472,7 @@ void iguana_parsebuf(struct iguana_info *coin,struct iguana_peer *addr,struct ig
     if ( memcmp(&checkH,H,sizeof(checkH)) == 0 )
     {
         //if ( strcmp(addr->ipaddr,"127.0.0.1") == 0 )
-        //printf("%s parse.(%s) len.%d\n",addr->ipaddr,H->command,len);
+        printf("%s parse.(%s) len.%d\n",addr->ipaddr,H->command,len);
         //printf("addr->dead.%u\n",addr->dead);
         if ( strcmp(H->command,"block") == 0 || strcmp(H->command,"tx") == 0 )
         {
