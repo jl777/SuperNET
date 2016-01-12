aba79dc393d23dbd31a59b14d5f93b5f65004c09
jl777 <jameslee777@yahoo.com>
2016-01-12 10:19:09 -0300

    tst

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -676,9 +676,9 @@ int32_t iguana_pollQsPT(struct iguana_info *coin,struct iguana_peer *addr)
     //if ( addr->pendblocks >= limit )
     //    printf("%s %d overlimit.%d\n",addr->ipaddr,addr->pendblocks,limit);
     req = queue_dequeue(&coin->priorityQ,0);
-    if ( 0 && coin->bundlescount > 0 && req == 0 )//addr->pendblocks < limit )//&& now > addr->lastpoll )
+    if ( 1 && coin->bundlescount > 0 && req == 0 )//addr->pendblocks < limit )//&& now > addr->lastpoll )
     {
-        if ( 0 )//strcmp("BTC",coin->symbol) != 0 )
+        if ( 1 )//strcmp("BTC",coin->symbol) != 0 )
         {
             int32_t bundlei;
             incr = coin->peers.numranked == 0 ? coin->MAXPEERS : coin->peers.numranked;
