6707bd7bc93899c069ed15da028478d3a56c9287
jl777 <jameslee777@yahoo.com>
2016-01-12 19:19:00 -0300

    tst

--- iguana/iguana_peers.c
+++ iguana/iguana_peers.c
@@ -992,7 +992,7 @@ void iguana_dedicatedloop(struct iguana_info *coin,struct iguana_peer *addr)
                     }
                 }
             }
-            if ( flag == 0 )
+            if ( 0 && flag == 0 )
             {
                 if ( run++ > 1000 )
                 {
