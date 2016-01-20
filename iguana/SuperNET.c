2b7fa53a80d0472e0c4b31bfe5ba5fea9c22c0d0
jl777 <jameslee777@yahoo.com>
2016-01-20 03:28:56 -0300

    test

--- iguana/SuperNET.c
+++ iguana/SuperNET.c
@@ -542,7 +542,8 @@ char *SuperNET_p2p(struct iguana_info *coin,struct iguana_peer *addr,int32_t *de
         if ( addr->validpub > 3 && addr->othervalid > 3 )
         {
             privkey = myinfo->privkey;
-            senderpub = addr->pubkey;
+            //senderpub = addr->pubkey;
+            memset(senderpub.bytes,0,sizeof(senderpub));
             printf("decrypt with priv.%llx pub.%llx\n",(long long)privkey.txid,(long long)senderpub.txid);
         } else privkey = GENESIS_PRIVKEY, senderpub = GENESIS_PUBKEY;
         if ( (msgbits= SuperNET_deciphercalc(&ptr,&msglen,privkey,senderpub,data,datalen,space,sizeof(space))) == 0 )
