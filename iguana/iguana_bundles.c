afa2e086b2bc93dd57d1fa952d8171e26d4640e6
jl777 <jameslee777@yahoo.com>
2016-03-07 00:37:06 -0300

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -144,9 +144,9 @@ int32_t iguana_hash2set(struct iguana_info *coin,char *debugstr,struct iguana_bu
     {
         char str2[65],str3[65];
         bits256_str(str2,*orighash2p), bits256_str(str3,newhash2);
-        printf("ERRRO iguana_hash2set overwrite [%s] %s with %s [%d:%d]\n",debugstr,str2,str3,bp->hdrsi,bundlei);
+        printf("WARNING iguana_hash2set overwrite [%s] %s with %s [%d:%d]\n",debugstr,str2,str3,bp->hdrsi,bundlei);
         *orighash2p = newhash2;
-        getchar();
+        //getchar();
         return(-1);
     }
     if ( isinside != 0 )
