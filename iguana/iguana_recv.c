1a5278258fa3554b877d0a6f1cf29629ade23fc7
jl777 <jameslee777@yahoo.com>
2016-03-09 19:03:46 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -946,9 +946,9 @@ int32_t iguana_pollQsPT(struct iguana_info *coin,struct iguana_peer *addr)
         return(0);
     }
     priority = 1;
-    if ( (req= queue_dequeue(&coin->priorityQ,0)) == 0 && (rand() % 10) == 0 )
+    if ( (req= queue_dequeue(&coin->priorityQ,0)) == 0 )
     {
-        if ( 1 || (rand() & 1) == 0 )
+        if ( (rand() & 1) == 0 )
             flag = iguana_neargap(coin,addr);
         else if ( (bp= addr->bp) != 0 && bp->rank != 0 && addr->pendblocks < limit )
         {
