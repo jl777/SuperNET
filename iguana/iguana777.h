5dd27fecdb942910bb18a13ce4970e00b65e3431
jl777 <jameslee777@yahoo.com>
2016-03-11 16:05:15 -0300

    test

--- iguana/iguana777.h
+++ iguana/iguana777.h
@@ -37,9 +37,9 @@ typedef int32_t (*blockhashfunc)(uint8_t *blockhashp,uint8_t *serialized,int32_t
 #define IGUANA_HEADPERCENTAGE 0.
 #define IGUANA_TAILPERCENTAGE .9
 #define IGUANA_MAXPENDHDRS 1
-#define _IGUANA_MAXPENDING 77
+#define _IGUANA_MAXPENDING 7
 #define _IGUANA_MAXBUNDLES 8 
-#define IGUANA_BUNDLELOOP 777
+#define IGUANA_BUNDLELOOP 7
 #define IGUANA_RPCPORT 7778
 #define IGUANA_MAXRAMCHAINSIZE ((uint64_t)1024L * 1024L * 1024L * 16)
 
