a1d68020787ea238fc81f26de86c59c36037d06d
jl777 <jameslee777@yahoo.com>
2016-03-12 20:50:26 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -501,7 +501,7 @@ int32_t iguana_bundleiters(struct iguana_info *coin,struct iguana_bundle *bp,int
         {
             if ( (block= bp->blocks[i]) != 0 )
             {
-                if ( block->RO.recvlen == 0 )//&& (block->fpipbits == 0 || block->fpos < 0) && block->queued == 0 )
+                if ( block->RO.recvlen == 0 || block->fpipbits == 0 || block->fpos < 0 )
                 {
                     if (  bp->issued[i] == 0 || now > bp->issued[i]+10 )
                     {
@@ -533,7 +533,7 @@ int32_t iguana_bundleiters(struct iguana_info *coin,struct iguana_bundle *bp,int
         //usleep(10000);
     }
     width = 100 + max*100;//sqrt(sqrt(bp->n * (1+bp->numsaved+issued)) * (10+coin->bundlescount-bp->hdrsi));
-    if ( 0 && bp->hdrsi == starti )
+    if ( 1 && bp->hdrsi == starti )
         printf("ITERATE.%d max.%d bundle.%d h.%d n.%d r.%d s.%d F.%d I.%d T.%d %f %u next %f counter.%d\n",bp->rank,max,bp->bundleheight/coin->chain->bundlesize,bp->numhashes,bp->n,bp->numrecv,bp->numsaved,bp->emitfinish,issued,timelimit,endmillis-OS_milliseconds(),(uint32_t)time(NULL),width,counter);
     if ( bp->emitfinish == 0 )
     {
