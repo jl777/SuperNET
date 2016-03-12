a865175b24f61c5a301a1174c43f2afa6eb5e885
jl777 <jameslee777@yahoo.com>
2016-03-12 01:36:55 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -1088,7 +1088,7 @@ int32_t iguana_reqblocks(struct iguana_info *coin)
     int32_t hdrsi,lflag,n,numissued,bundlei,flag = 0; bits256 hash2; struct iguana_block *next,*block; struct iguana_bundle *bp; struct iguana_peer *addr;
     if ( 1 && (bp= coin->current) != 0 && bp->numsaved < bp->n ) // queue_size(&coin->priorityQ) == 0 &&
     {
-        for (hdrsi=numissued=0; hdrsi<coin->peers.numranked && coin->current->hdrsi+hdrsi<coin->bundlescount && numissued<1; hdrsi++)
+        for (hdrsi=numissued=0; hdrsi<coin->peers.numranked && coin->current->hdrsi+hdrsi<coin->bundlescount && numissued<100; hdrsi++)
         {
             if ( (bp= coin->bundles[hdrsi + coin->current->hdrsi]) == 0 )
                 continue;
@@ -1113,7 +1113,7 @@ int32_t iguana_reqblocks(struct iguana_info *coin)
                         iguana_sendblockreqPT(coin,addr,bp,bundlei,block->RO.hash2,0);
                         //iguana_blockQ(coin,bp,bundlei,block->RO.hash2,1);
                         flag++;
-                        if ( ++numissued > 0 )
+                        if ( ++numissued > 100 )
                             break;
                     }
                 }
