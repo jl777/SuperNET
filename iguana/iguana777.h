c8620d555df8c3ddd07b8378d5ee11cfae7329bc
jl777 <jameslee777@yahoo.com>
2016-03-12 00:53:48 -0300

    test

--- iguana/iguana777.h
+++ iguana/iguana777.h
@@ -111,7 +111,7 @@ extern int32_t IGUANA_NUMHELPERS;
 #define NODE_GETUTXO (1 << 1)
 #define NODE_BLOOM (1 << 2)
 
-#define PROTOCOL_VERSION 70012
+#define PROTOCOL_VERSION 70001
 #define INIT_PROTO_VERSION 209 // initial proto version, to be increased after version/verack negotiation
 #define GETHEADERS_VERSION 31800 // In this version, 'getheaders' was introduced.
 #define MIN_PEER_PROTO_VERSION GETHEADERS_VERSION // disconnect from peers older than this proto version
