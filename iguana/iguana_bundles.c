04e5f254e8b45d927282ce44194630b2c504f6f7
jl777 <jameslee777@yahoo.com>
2016-03-17 20:31:54 -0300

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -533,7 +533,7 @@ int64_t iguana_bundlecalcs(struct iguana_info *coin,struct iguana_bundle *bp)
                             bp->numcached++;
                     }
                 }
-                else
+                else if ( 0 )
                 {
                     printf("clear block?\n");
                     block->RO.recvlen = 0;
