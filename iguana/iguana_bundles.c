a4d3ef4a9e4691611f437ca4f013983575f3d53f
jl777 <jameslee777@yahoo.com>
2016-03-22 03:10:00 -0300

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -629,7 +629,7 @@ int32_t iguana_bundlehdr(struct iguana_info *coin,struct iguana_bundle *bp,int32
                     //printf("speculative.[%d:%d]\n",bp->hdrsi,i);
                     iguana_blockQ("speculative",coin,0,-1,bp->speculative[i],0);
                 }
-                break;
+                //break;
             }
            // else if ( bp->blocks[i] == 0 )
            //     break;
