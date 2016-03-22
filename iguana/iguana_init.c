834d6238562e7ec347a4fe68c5b7c1e553225899
jl777 <jameslee777@yahoo.com>
2016-03-22 04:28:27 -0300

    test

--- iguana/iguana_init.c
+++ iguana/iguana_init.c
@@ -279,7 +279,7 @@ void iguana_parseline(struct iguana_info *coin,int32_t iter,FILE *fp)
                                 //printf("LOADED bundle.%d %p current %p\n",bp->bundleheight,bp,coin->current);
                                 //if ( bp->hdrsi == 0 || coin->bundles[bp->hdrsi-1]->emitfinish != 0 )
                                 {
-                                    if ( 1 )
+                                    if ( 0 )
                                         bp->validated = bp->balancefinish = bp->startutxo = bp->utxofinish = (uint32_t)time(NULL);
                                     //printf("GENERATE UTXO, verify sigs, etc for ht.%d\n",bp->bundleheight);
                                     iguana_bundleQ(coin,bp,1000);
