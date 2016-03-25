3bc504a1ebf0e919a76bff2f0f6b5d5516033b05
jl777 <jameslee777@yahoo.com>
2016-03-24 23:01:14 -0300

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -769,7 +769,9 @@ int64_t iguana_bundlecalcs(struct iguana_info *coin,struct iguana_bundle *bp)
 int32_t iguana_bundlefinish(struct iguana_info *coin,struct iguana_bundle *bp)
 {
     struct iguana_bundle *prevbp; int32_t i,retval;
+#ifndef IGUANA_SERIALIZE_BALANCEGEN 
     if ( coin->MAXMEM <= 4*(1024L * 1024 * 1024) )
+#endif
     {
         if ( (prevbp= coin->current) != 0 && prevbp->hdrsi < (coin->longestchain / coin->chain->bundlesize)-0*coin->MAXBUNDLES )
             return(0);
