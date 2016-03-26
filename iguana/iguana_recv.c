2936c6ae30b8fce27808789c491dfddb009b1ff1
jl777 <jameslee777@yahoo.com>
2016-03-26 00:14:18 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -513,10 +513,9 @@ struct iguana_bundle *iguana_bundleset(struct iguana_info *coin,struct iguana_bl
 
 void iguana_checklongestchain(struct iguana_info *coin,struct iguana_bundle *bp,int32_t num)
 {
-    printf("strange.%d suspicious longestchain.%d vs [%d:%d] %d bp->n %d\n",coin->longestchain_strange,coin->longestchain,bp->hdrsi,num,bp->bundleheight+num,bp->n);
-    if ( num > 10 && num < bp->n && coin->longestchain > bp->bundleheight+bp->n )
+    if ( num > 10 && num < bp->n && coin->longestchain > bp->bundleheight+num+3 )
     {
-        printf("suspicious longestchain.%d vs [%d:%d] %d\n",coin->longestchain,bp->hdrsi,num,bp->bundleheight+num);
+        printf("strange.%d suspicious longestchain.%d vs [%d:%d] %d bp->n %d\n",coin->longestchain_strange,coin->longestchain,bp->hdrsi,num,bp->bundleheight+num,bp->n);
         if ( coin->longestchain_strange++ > 10 )
         {
             coin->badlongestchain = coin->longestchain;
