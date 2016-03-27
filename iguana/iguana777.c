cb03766a8ec60e3db48be5a730a47d942ef734c3
jl777 <jameslee777@yahoo.com>
2016-03-26 21:32:35 -0300

    test

--- iguana/iguana777.c
+++ iguana/iguana777.c
@@ -412,7 +412,7 @@ void iguana_helper(void *arg)
         {
             if ( (ptr= queue_dequeue(&validateQ,0)) != 0 )
             {
-                if ( ptr->bp != 0 && ptr->coin != 0 && coin->active != 0 )
+                if ( ptr->bp != 0 && (coin= ptr->coin) != 0 && coin->active != 0 )
                     flag += iguana_bundlevalidate(ptr->coin,ptr->bp);
                 else printf("helper validate missing param? %p %p\n",ptr->coin,ptr->bp);
                 myfree(ptr,ptr->allocsize);
