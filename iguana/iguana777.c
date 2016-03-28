5da5ff23e02b14d7bc632714f2531d0b86146a1d
jl777 <jameslee777@yahoo.com>
2016-03-27 23:14:11 -0300

    test

--- iguana/iguana777.c
+++ iguana/iguana777.c
@@ -401,13 +401,13 @@ void iguana_helper(void *arg)
             if ( (bp= ptr->bp) != 0 && (coin= ptr->coin) != 0 && coin->active != 0 )
             {
                 coin->numbundlesQ--;
-                if ( coin->started != 0 && time(NULL) >= bp->nexttime )
+                if ( coin->started != 0 )//&& time(NULL) >= bp->nexttime )
                     flag += iguana_bundleiters(ptr->coin,&MEM,MEMB,bp,ptr->timelimit);
                 else
                 {
                     if ( 0 && bp != 0 )
                         printf("skip.%d lag.%ld\n",bp->hdrsi,time(NULL)-bp->nexttime);
-                        iguana_bundleQ(ptr->coin,bp,1000);
+                    iguana_bundleQ(ptr->coin,bp,1000);
                 }
                 if ( coin->current != 0 && coin->current->hdrsi != coin->bundlescount-1 )
                     allcurrent = 0;
