3bdb6c7c33300a606ff2e919a727eafaec396c33
jl777 <jameslee777@yahoo.com>
2016-03-30 07:39:07 -0300

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -474,7 +474,7 @@ int32_t iguana_blocksmissing(struct iguana_info *coin,int32_t *nonzp,uint8_t mis
 
 int32_t iguana_sendhashes(struct iguana_info *coin,struct iguana_peer *addr,int32_t msgtype,bits256 hashes[],int32_t n)
 {
-    int32_t len; uint8_t *serialized = malloc((sizeof(int32_t) + sizeof(*hashes))*n + 1024);
+    /*int32_t len; uint8_t *serialized = malloc((sizeof(int32_t) + sizeof(*hashes))*n + 1024);
     if ( (len= iguana_getdata(coin,serialized,MSG_BLOCK,hashes,n)) > 0 )
     {
         if ( len > (sizeof(int32_t) + sizeof(*hashes))*n + 1024 )
@@ -488,7 +488,9 @@ int32_t iguana_sendhashes(struct iguana_info *coin,struct iguana_peer *addr,int3
         addr->pendtime = (uint32_t)time(NULL);
         //printf("sendhashes[%d] -> %s\n",n,addr->ipaddr);
     } else n = 0;
-    free(serialized);
+    free(serialized);*/
+    int32_t i; for (i=0; i<n; i++)
+        iguana_blockQ("test",coin,0,-1,hashes[i],0);
     return(n);
 }
 
