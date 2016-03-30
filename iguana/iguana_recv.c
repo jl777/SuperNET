6480052ddf7e7164f6842491285d38f7a7f055a4
jl777 <jameslee777@yahoo.com>
2016-03-30 02:50:02 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -287,7 +287,7 @@ void iguana_gotblockM(struct iguana_info *coin,struct iguana_peer *addr,struct i
             uint64_t sum2 = 0,sum = 0;
             for (i=0; i<sizeof(received)/sizeof(*received); i++)
                 sum += received[i], sum2 += count[i];
-            printf("TOTAL BLOCKS.%llu RECEIVED %s ave %.1f | duplicates.%d %lld afteremit.%d %lld\n",(long long)sum2,mbstr(str,sum),(double)sum/(sum2!=0?sum2:1),numDuplicates,(long long)sizeDuplicates,numAfteremit,(long long)sizeAfteremit);
+            char str[65],str2[65]; printf("TOTAL BLOCKS.%llu RECEIVED %s ave %.1f | duplicates.%d %s afteremit.%d %s\n",(long long)sum2,mbstr(str,sum),(double)sum/(sum2!=0?sum2:1),numDuplicates,mbstr(str,sizeDuplicates),numAfteremit,mbstr(str2,sizeAfteremit));
         }
     }
     copyflag = 0;//(coin->enableCACHE != 0) && (strcmp(coin->symbol,"BTC") != 0);
