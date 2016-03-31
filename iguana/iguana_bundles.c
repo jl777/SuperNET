41d4e5fac0df622366538841aa9fe8aacd2b8434
jl777 <jameslee777@yahoo.com>
2016-03-31 12:12:55 -0600

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -806,7 +806,7 @@ int32_t iguana_bundleiters(struct iguana_info *coin,struct OS_memspace *mem,stru
                         while ( (breq= queue_dequeue(&coin->priorityQ,0)) != 0 )
                             myfree(breq,sizeof(*breq));
                     }
-                    n = iguana_blocksmissing(coin,&avail,missings,0,bp,bp->n,0*coin->MAXSTUCKTIME);
+                    n = iguana_blocksmissing(coin,&avail,missings,0,bp,bp->n,30);
                     printf("issued %d priority requests [%d] to unstick stuckiters.%d lag.%d\n",n,bp->hdrsi,coin->stuckiters,lag);
                     /*for (i=n=0; i<bp->n; i++)
                     {
