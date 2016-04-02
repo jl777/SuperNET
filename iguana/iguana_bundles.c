17751c635d17c356649c4487036272ee6f531610
jl777 <jameslee777@yahoo.com>
2016-04-01 23:27:30 -0600

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -635,7 +635,7 @@ int32_t iguana_bundlehdr(struct iguana_info *coin,struct iguana_bundle *bp,int32
     {
         char str[64];
         bp->hdrtime = (uint32_t)time(NULL);
-        if ( bp == coin->current && bp->speculative != 0 )
+        if ( bp == coin->current )//&& bp->speculative != 0 )
         {
             //printf("iguana_bundlehdr.[%d] %d %s\n",bp->hdrsi,bp->numspec,bits256_str(str,bp->hashes[0]));
             if ( iguana_blocksmissing(coin,&avail,missings,0,bp,0,3) > 0 )
