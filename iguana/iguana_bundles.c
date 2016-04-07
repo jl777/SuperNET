faac6f6573328590fd851661f322aeb0d0bb72f8
jl777 <jameslee777@yahoo.com>
2016-04-07 17:56:43 -0500

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -549,10 +549,9 @@ int32_t iguana_bundleissuemissing(struct iguana_info *coin,struct iguana_bundle 
                 }
             }
         }
-        if ( firsti >= 0 ) //firsti == lasti &&
+        if ( firsti >= 0 && firsti == lasti )
         {
-            if( bp == coin->current )
-                printf("[%d] first missing.%d of %d\n",bp->hdrsi,firsti,nonz);
+            printf("[%d] first missing.%d of %d\n",bp->hdrsi,firsti,nonz);
             iguana_bundleblock(coin,&hash2,bp,firsti);
             if ( bits256_nonz(hash2) != 0 )
             {
