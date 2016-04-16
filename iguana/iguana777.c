0a229d44e2d4598ccf975a290f29403af6050921
jl777 <jameslee777@yahoo.com>
2016-04-16 05:49:24 -0500

    test

--- iguana/iguana777.c
+++ iguana/iguana777.c
@@ -360,9 +360,9 @@ int32_t iguana_validated(struct iguana_info *coin)
 int32_t iguana_utxogen(struct iguana_info *coin,int32_t helperid,int32_t convertflag)
 {
     int32_t hdrsi,retval,n,max,incr,num = 0; struct iguana_bundle *bp;
-    incr = IGUANA_NUMHELPERS;//(coin->PREFETCHLAG > 0) ? 1 : IGUANA_NUMHELPERS;
-    //if ( coin->PREFETCHLAG >= 0 && helperid != 0 )
-    //    return(0);
+    incr = (coin->PREFETCHLAG > 0) ? 1 : IGUANA_NUMHELPERS;
+    if ( coin->PREFETCHLAG > 0 && helperid != 0 )
+        return(0);
     max = coin->bundlescount;
     if ( coin->bundles[max-1] != 0 && coin->bundles[max-1]->emitfinish <= 1 )
         max--;
