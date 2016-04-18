3e2e7029e4700d8faf3ddc518999cce4400f4e1b
jl777 <jameslee777@yahoo.com>
2016-04-17 21:00:45 -0500

    test

--- iguana/iguana_unspents.c
+++ iguana/iguana_unspents.c
@@ -1848,7 +1848,7 @@ int32_t iguana_realtime_update(struct iguana_info *coin)
             {
                 printf("error generating spendvectors.[%d], exiting. just restart iguana\n",i);
                 exit(-1);
-            } else printf("generated UTXO.[%d]\n",i);
+            } // else printf("generated UTXO.[%d]\n",i);
             coin->spendvectorsaved = 1;
         }
     }
@@ -1876,7 +1876,7 @@ int32_t iguana_realtime_update(struct iguana_info *coin)
                 }
             }
         }
-        char str[65]; printf("check longest.%d RTheight.%d hwm.%d %s %p\n",coin->longestchain,coin->RTheight,coin->blocks.hwmchain.height,bits256_str(str,bp->hashes[0]),block);
+        //char str[65]; printf("check longest.%d RTheight.%d hwm.%d %s %p\n",coin->longestchain,coin->RTheight,coin->blocks.hwmchain.height,bits256_str(str,bp->hashes[0]),block);
         if ( bits256_cmp(coin->RThash1,bp->hashes[1]) != 0 )
             coin->RThash1 = bp->hashes[1];
         bp->lastRT = (uint32_t)time(NULL);
@@ -1953,7 +1953,7 @@ int32_t iguana_realtime_update(struct iguana_info *coin)
     n = 0;
     if ( dest != 0 && flag != 0 && coin->RTheight >= coin->longestchain )
     {
-        printf("ramchainiterate.[%d] ave %.2f micros, total %.2f seconds starti.%d num.%d\n",num0,(totalmillis0*1000.)/num0,totalmillis0/1000.,coin->RTstarti,coin->RTheight%bp->n);
+        //printf("ramchainiterate.[%d] ave %.2f micros, total %.2f seconds starti.%d num.%d\n",num0,(totalmillis0*1000.)/num0,totalmillis0/1000.,coin->RTstarti,coin->RTheight%bp->n);
         if ( (n= iguana_walkchain(coin,1)) == coin->RTheight-1 )
         {
             printf("RTgenesis verified\n");
