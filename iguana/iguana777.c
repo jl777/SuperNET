5ab17ede09e8e0701d3b271f9947a195547f614e
jl777 <jameslee777@yahoo.com>
2016-04-20 00:42:15 -0500

    test

--- iguana/iguana777.c
+++ iguana/iguana777.c
@@ -417,7 +417,7 @@ int32_t iguana_utxogen(struct iguana_info *coin,int32_t helperid,int32_t convert
         return(0);
     }
     incr = IGUANA_NUMHELPERS;
-    if ( 1 || coin->PREFETCHLAG > 0 )
+    if ( 1 || coin->PREFETCHLAG > 0 ) // data issues on slow systems
         incr = 1;
     max = coin->bundlescount;
     if ( coin->bundles[max-1] != 0 && coin->bundles[max-1]->emitfinish <= 1 )
