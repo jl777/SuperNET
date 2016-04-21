99d3f033712359e575d1ac70ba6f1bc0563269b1
jl777 <jameslee777@yahoo.com>
2016-04-20 21:56:30 -0500

    test

--- iguana/SuperNET.c
+++ iguana/SuperNET.c
@@ -707,16 +707,15 @@ char *SuperNET_JSON(struct supernet_info *myinfo,cJSON *json,char *remoteaddr,ui
         if ( (retstr= SuperNET_processJSON(myinfo,json,remoteaddr,port)) != 0 )
         {
             //printf("retstr.(%s)\n",retstr);
-            if ( retstr[strlen(retstr)-1] != '\n' && (retjson= cJSON_Parse(retstr)) != 0 && is_cJSON_Array(retjson) == 0 )
+            /*if ( retstr[strlen(retstr)-1] != '\n' && (retjson= cJSON_Parse(retstr)) != 0 && is_cJSON_Array(retjson) == 0 )
             {
                 if ( jobj(retjson,"result") == 0 || jobj(retjson,"error") != 0 || jobj(retjson,"method") != 0 )
                 {
-                    //printf("it is a result, dont return\n");
                     free(retstr);
                     retstr = 0;
                 }
                 free_json(retjson);
-            }
+            }*/
         } else printf("null retstr from SuperNET_JSON\n");
     }
     if ( flag != 0 && hexmsg != 0 && hexmsg != hexbuf )
