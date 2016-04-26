9bf036c61e1831eec8de9ff06b46b45ead2ee140
jl777 <jameslee777@yahoo.com>
2016-04-25 23:45:46 -0500

    test

--- iguana/iguana_unspents.c
+++ iguana/iguana_unspents.c
@@ -2416,9 +2416,9 @@ int32_t iguana_realtime_update(struct iguana_info *coin)
     bp = coin->current;
     if ( bp == 0 || iguana_validated(coin) < bp->hdrsi )
         return(0);
-    if ( 1 && coin->RTheight > 0 && coin->spendvectorsaved != 1 && coin->bundlescount != coin->balanceswritten )
+    if ( 1 && coin->RTheight > 0 && coin->spendvectorsaved != 1 && coin->bundlescount-1 != coin->balanceswritten )
     {
-        printf("RT mismatch %d != %d\n",coin->bundlescount,coin->balanceswritten);
+        printf("RT mismatch %d != %d\n",coin->bundlescount-1,coin->balanceswritten);
         coin->spendvectorsaved = 0;
         iguana_RTramchainfree(coin,coin->current);
         return(0);
