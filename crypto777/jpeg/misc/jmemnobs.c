025c077fd0fa5506a7ad7e77e15a23e3ad0e772d
jl777 <jameslee777@yahoo.com>
2016-05-13 17:52:59 -0500

    test

--- crypto777/jpeg/misc/jmemnobs.c
+++ crypto777/jpeg/misc/jmemnobs.c
@@ -16,9 +16,9 @@
  */
 
 #define JPEG_INTERNALS
-#include "jinclude.h"
-#include "jpeglib.h"
-#include "jmemsys.h"		/* import the system-dependent declarations */
+#include "../jinclude.h"
+#include "../jpeglib.h"
+#include "../jmemsys.h"		/* import the system-dependent declarations */
 
 #ifndef HAVE_STDLIB_H		/* <stdlib.h> should declare malloc(),free() */
 extern void * malloc JPP((size_t size));
