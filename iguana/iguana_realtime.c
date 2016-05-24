05107681884f5c8f317375764cbc2e6d4a1a3ea2
jl777 <jameslee777@yahoo.com>
2016-05-24 13:33:13 -0500

    test

--- iguana/iguana_realtime.c
+++ iguana/iguana_realtime.c
@@ -334,7 +334,7 @@ int32_t iguana_realtime_update(struct iguana_info *coin)
     if ( coin->RTdatabad == 0 && dest != 0 && flag != 0 && coin->RTheight >= coin->longestchain-offset )
     {
         //printf("ramchainiterate.[%d] ave %.2f micros, total %.2f seconds starti.%d num.%d\n",num0,(totalmillis0*1000.)/num0,totalmillis0/1000.,coin->RTstarti,coin->RTheight%bp->n);
-        if ( (n= iguana_walkchain(coin,1)) == coin->RTheight-1 )
+        if ( (n= iguana_walkchain(coin,1)) == coin->RTheight-1-offset )
         {
             //printf("RTgenesis verified\n");
             if ( (coin->RTheight % coin->chain->bundlesize) > 1 )
