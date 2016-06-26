697c0e505945f93c505cd5b7689a7e427b6ff005
jl777 <jameslee777@yahoo.com>
2016-06-26 17:29:09 -0300

    test

--- iguana/iguana_realtime.c
+++ iguana/iguana_realtime.c
@@ -213,7 +213,7 @@ int32_t iguana_realtime_update(struct supernet_info *myinfo,struct iguana_info *
     {
         ;
     }
-    offset = (strcmp("BTC",coin->symbol) != 0);
+    offset = (strcmp("BTCD",coin->symbol) == 0);
     if ( coin->current != 0 && (coin->blocks.hwmchain.height % coin->chain->bundlesize) == coin->chain->bundlesize-1 && coin->blocks.hwmchain.height/coin->chain->bundlesize == coin->longestchain/coin->chain->bundlesize )
     {
         block = coin->current->blocks[coin->current->n - 1];
@@ -344,7 +344,7 @@ int32_t iguana_realtime_update(struct supernet_info *myinfo,struct iguana_info *
                 flag++;
                 //coin->blocks.RO[bp->bundleheight+bundlei] = block->RO;
                 coin->RTheight++;
-                //printf(">>>> RT.%d hwm.%d L.%d T.%d U.%d S.%d P.%d X.%d -> size.%ld\n",coin->RTheight,coin->blocks.hwmchain.height,coin->longestchain,dest->H.txidind,dest->H.unspentind,dest->H.spendind,dest->pkind,dest->externalind,(long)dest->H.data->allocsize);
+                printf(">>>> RT.%d hwm.%d L.%d T.%d U.%d S.%d P.%d X.%d -> size.%ld\n",coin->RTheight,coin->blocks.hwmchain.height,coin->longestchain,dest->H.txidind,dest->H.unspentind,dest->H.spendind,dest->pkind,dest->externalind,(long)dest->H.data->allocsize);
                 if ( coin->RTramchain.H.data != 0 )
                     coin->RTramchain.H.data->numblocks = bundlei + 1;
                 else break;
