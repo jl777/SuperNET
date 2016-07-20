21c859e80bb1b99bff967ce74ad2c089535e066d
jl777 <jameslee777@yahoo.com>
2016-07-20 15:55:31 -0300

    test

--- iguana/iguana_unspents.c
+++ iguana/iguana_unspents.c
@@ -395,7 +395,7 @@ int64_t iguana_pkhashbalance(struct supernet_info *myinfo,struct iguana_info *co
     if ( ramchain->Uextras == 0 || (rdata= ramchain->H.data) == 0 )
     {
         if ( ramchain->height < (coin->bundlescount-1)*coin->chain->bundlesize )
-            printf("iguana_pkhashbalance.[%d]: unexpected null spents.%p or rdata.%p\n",ramchain->height,ramchain->Uextras,rdata);
+            printf("iguana_pkhashbalance.[%d] %d: unexpected null spents.%p or rdata.%p\n",ramchain->height,(coin->bundlescount-1)*coin->chain->bundlesize,ramchain->Uextras,rdata);
         else iguana_volatilesalloc(coin,ramchain,0);
         return(0);
     }
