9d7c1672bb6a40ce76e695f2d4279d348ec29cbd
jl777 <jameslee777@yahoo.com>
2016-07-20 16:37:54 -0300

    test

--- iguana/iguana_spendvectors.c
+++ iguana/iguana_spendvectors.c
@@ -725,7 +725,7 @@ void iguana_initfinal(struct iguana_info *coin,bits256 lastbundle)
         if ( i == 0 )
             bp->utxofinish = bp->startutxo = (uint32_t)time(NULL);
     }
-    if ( i < coin->bundlescount-1 )
+    if ( 1 || i < coin->bundlescount-1 )
     {
         printf("spendvectors.[%d] max.%d missing, will regen all of them\n",i,coin->bundlescount-1);
         for (i=0; i<coin->bundlescount-1; i++)
@@ -745,9 +745,10 @@ void iguana_initfinal(struct iguana_info *coin,bits256 lastbundle)
     printf("i.%d bundlescount.%d\n",i,coin->bundlescount);
     if ( coin->balanceswritten > 1 )
         coin->balanceswritten = iguana_volatilesinit(coin);
-    if ( coin->balanceswritten > 1 )
+    //if ( coin->balanceswritten > 1 )
     {
-        for (i=0; i<coin->balanceswritten; i++)
+        //for (i=0; i<coin->balanceswritten; i++)
+        for (i=0; i<coin->bundlescount; i++)
         {
             //printf("%d ",i);
             iguana_validateQ(coin,coin->bundles[i]);
