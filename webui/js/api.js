805ae239d9bfa374a97a17937a8d26c4123a34b5
pbca26 <pbca26@gmail.com>
2016-07-31 12:39:48 -0400

    T467, T503 Fixed bugs, added create account api call

--- webui/js/api.js
+++ webui/js/api.js
@@ -3,18 +3,22 @@
  *
  */
 
-var protocol = "http://";
-var serverName = "localhost";
-var serverPort = "7778";
+var server = {
+  "protocol": "http://",
+  "ip": "localhost",
+  "port": "7778"
+};
 var apiRoutes = {
   "bitcoinRPC" : {
-    "walletPassphrase" : "bitcoinrpc/walletpassphrase"
+    "walletPassphrase" : "bitcoinrpc/walletpassphrase", // params: password String, timeout Int
+    "encryptWallet" : "bitcoinrpc/encryptwallet" // params: passphrase String
   }
 };
 
 function getServerUrl() {
-  return protocol + serverName + ":" + serverPort + '/api/';
+  return server.protocol + server.ip + ":" + server.port + "/api/";
 }
+
 function walletLogin(passphrase) {
   var result = false;
 
@@ -24,11 +28,40 @@ function walletLogin(passphrase) {
     dataType: "text",
     async: false
   })
-  .done(function(_response){
+  .done(function(_response) {
     var response = $.parseJSON(_response);
+
     if (response.error) {
       // do something
-      console.log('error: ' + response.error);
+      console.log("error: " + response.error);
+      result = false;
+    } else {
+      if (response.result === "success") {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  });
+
+  return result;
+}
+
+function walletCreate(passphrase) {
+  var result = false;
+
+  $.ajax({
+    url: getServerUrl() + apiRoutes.bitcoinRPC.encryptWallet + "?passphrase=" + passphrase,
+    cache: false,
+    dataType: "text",
+    async: false
+  })
+  .done(function(_response) {
+    var response = $.parseJSON(_response);
+
+    if (response.error) {
+      // do something
+      console.log("error: " + response.error);
       result = false;
     } else {
       if (response.result === "success") {
