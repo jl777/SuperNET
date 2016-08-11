c4388d6def138e015ccc4ad8c70bf3b1c7f1c37a
jl777 <jameslee777@yahoo.com>
2016-08-11 09:59:19 -0300

    test

--- iguana/iguana_realtime.c
+++ iguana/iguana_realtime.c
@@ -210,12 +210,12 @@ int32_t iguana_realtime_update(struct supernet_info *myinfo,struct iguana_info *
     struct iguana_block *block=0; struct iguana_blockRO *B; struct iguana_ramchain *dest=0,blockR;
     if ( coin->peers == 0 && coin->virtualchain == 0 )
         return(0);
-    if ( coin->RTheight > coin->blocks.hwmchain.height )
+    offset = (strcmp("BTC",coin->symbol) != 0);
+    if ( coin->RTheight-offset > coin->blocks.hwmchain.height )
     {
         printf("inversion RT %d < %d\n",coin->RTheight,coin->blocks.hwmchain.height);
         coin->RTheight = 0;
     }
-    offset = (strcmp("BTC",coin->symbol) != 0);
     if ( coin->current != 0 && (coin->blocks.hwmchain.height % coin->chain->bundlesize) == coin->chain->bundlesize-1 && coin->blocks.hwmchain.height/coin->chain->bundlesize == coin->longestchain/coin->chain->bundlesize )
     {
         block = coin->current->blocks[coin->current->n - 1];
