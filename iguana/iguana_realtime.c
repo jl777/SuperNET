3da684cd4019c8e891d1232cd495c2cdbe7b4f44
jl777 <jameslee777@yahoo.com>
2016-08-14 16:10:04 -0300

    test

--- iguana/iguana_realtime.c
+++ iguana/iguana_realtime.c
@@ -274,7 +274,7 @@ int32_t iguana_realtime_update(struct supernet_info *myinfo,struct iguana_info *
         }*/
         return(0);
     }
-    if ( coin->RTdatabad == 0 && bp->hdrsi == coin->longestchain/coin->chain->bundlesize && bp->hdrsi >= coin->balanceswritten-1 && ((coin->RTheight < coin->blocks.hwmchain.height-offset && time(NULL) > bp->lastRT) || time(NULL) > bp->lastRT+1) ) //coin->RTheight >= bp->bundleheight && coin->RTheight < bp->bundleheight+bp->n &&
+    if ( coin->RTdatabad == 0 && bp->hdrsi == coin->longestchain/coin->chain->bundlesize && bp->hdrsi >= coin->balanceswritten-2 && ((coin->RTheight < coin->blocks.hwmchain.height-offset && time(NULL) > bp->lastRT) || time(NULL) > bp->lastRT+1) ) //coin->RTheight >= bp->bundleheight && coin->RTheight < bp->bundleheight+bp->n &&
     {
         if ( coin->RTheight == 0 )
             coin->RTheight = bp->bundleheight;
