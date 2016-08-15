43d981e8295f80ee3d00123a9d97ebc073d5e117
jl777 <jameslee777@yahoo.com>
2016-08-15 14:52:23 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -1070,7 +1070,7 @@ struct iguana_bundlereq *iguana_recvblockhashes(struct iguana_info *coin,struct 
 struct iguana_bundlereq *iguana_recvblock(struct iguana_info *coin,struct iguana_peer *addr,struct iguana_bundlereq *req,struct iguana_zblock *origblock,int32_t numtx,int32_t datalen,int32_t recvlen,int32_t *newhwmp)
 {
     struct iguana_bundle *bp=0,*prev; int32_t n,bundlei = -2; struct iguana_block *block,*next,*prevblock; char str[65]; bits256 hash2;
-    //printf("received.%s\n",bits256_str(str,origblock->RO.hash2));
+    printf("received.%s\n",bits256_str(str,origblock->RO.hash2));
     if ( bits256_nonz(origblock->RO.prev_block) != 0 )
     {
         if ( (prevblock= iguana_blockfind("prev",coin,origblock->RO.prev_block)) != 0 && prevblock->height+1 > coin->longestchain )
