96c9b3fff886a99f92339b658dc5c71627bd06c9
jl777 <jameslee777@yahoo.com>
2016-08-18 23:34:58 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -510,7 +510,10 @@ void iguana_gotblockM(struct iguana_info *coin,struct iguana_peer *addr,struct i
     if ( txdata->zblock.fpos == 0 )
     {
         numtx = origtxdata->zblock.RO.txn_count;
-        if ( coin->RTheight > 0 )
+        for (i=0; i<coin->bundlescount; i++)
+            if ( (bp= coin->bundles[i]) != 0 && bits256_cmp(bp->hashes[0],txdata->zblock.RO.hash2) == 0 )
+                break;
+        if ( i != coin->bundlescount || coin->RTheight > 0 )
         {
             portable_mutex_lock(&coin->RTmutex);
             iguana_RTrawdata(coin,txdata->zblock.RO.hash2,data,&recvlen,&numtx,0);
