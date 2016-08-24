6f17e06c2f9a1f64fae03bd9570603a9b0db3134
jl777 <jameslee777@yahoo.com>
2016-08-24 15:42:55 -0300

    test

--- iguana/iguana_unspents.c
+++ iguana/iguana_unspents.c
@@ -120,9 +120,9 @@ int32_t iguana_unspentindfind(struct supernet_info *myinfo,struct iguana_info *c
         unspentind = (tp->firstvout + vout);
     if ( coinaddr != 0 && unspentind > 0 && (hdrsi= *heightp/coin->chain->bundlesize) >= 0 && hdrsi < coin->bundlescount && (bp= coin->bundles[hdrsi]) != 0 && (rdata= bp->ramchain.H.data) != 0 && unspentind < rdata->numunspents )
     {
-        if ( bp->lastprefetch == 0 )
+        if ( time(NULL) > bp->lastprefetch+13 )
         {
-            iguana_ramchain_prefetch(coin,&bp->ramchain,2);
+            iguana_ramchain_prefetch(coin,&bp->ramchain,6);
             bp->lastprefetch = (uint32_t)time(NULL);
         }
         U = RAMCHAIN_PTR(rdata,Uoffset);
