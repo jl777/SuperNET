5210d08e287b809ff6d79bd7a0437284be61fe8a
jl777 <jameslee777@yahoo.com>
2016-08-25 16:00:49 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -1855,14 +1855,18 @@ int32_t iguana_processrecv(struct supernet_info *myinfo,struct iguana_info *coin
         coin->balanceflush = 0;
         //iguana_utxoaddr_gen(myinfo,coin,(coin->balanceswritten - 1) * coin->chain->bundlesize);
     }
-    if ( coin->utxoaddrtable != 0 && coin->RTheight > 0 && coin->RTheight <= coin->blocks.hwmchain.height )
+    if ( (rand() % 10) == 0 )
     {
-        struct iguana_block *block;
-        if ( (block= iguana_blockfind("utxogen",coin,coin->blocks.hwmchain.RO.hash2)) != 0 )
-            iguana_RTnewblock(myinfo,coin,block);
+        if ( coin->utxoaddrtable != 0 && coin->RTheight > 0 && coin->RTheight <= coin->blocks.hwmchain.height )
+        {
+            struct iguana_block *block;
+            if ( (block= iguana_blockfind("utxogen",coin,coin->blocks.hwmchain.RO.hash2)) != 0 )
+                iguana_RTnewblock(myinfo,coin,block);
+        }
     }
     flag += iguana_processrecvQ(myinfo,coin,&newhwm);
-    flag += iguana_reqblocks(myinfo,coin);
+    if ( (rand() % 10) == 0 )
+        flag += iguana_reqblocks(myinfo,coin);
     if ( time(NULL) > coin->laststats+30 )
     {
         flag += iguana_reqhdrs(coin);
