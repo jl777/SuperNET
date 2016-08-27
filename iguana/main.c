536b4d8fb809a077e31121799e33506192c27ae2
jl777 <jameslee777@yahoo.com>
2016-08-27 12:19:54 -0300

    test

--- iguana/main.c
+++ iguana/main.c
@@ -1505,6 +1505,10 @@ void iguana_relays_init(struct supernet_info *myinfo)
         free(str);
     if ( (str= basilisk_addrelay_info(myinfo,0,(uint32_t)calc_ipbits("89.248.160.238"),GENESIS_PUBKEY)) != 0 ) //"5.9.102.210"
         free(str);
+    if ( (str= basilisk_addrelay_info(myinfo,0,(uint32_t)calc_ipbits("89.248.160.239"),GENESIS_PUBKEY)) != 0 )
+        free(str);
+    if ( (str= basilisk_addrelay_info(myinfo,0,(uint32_t)calc_ipbits("89.248.160.240"),GENESIS_PUBKEY)) != 0 ) 
+        free(str);
 }
 
 void iguana_main(void *arg)
