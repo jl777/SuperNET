5b93cd9da3fecb32507ce7cae9cb2d9ad556dae6
pbca26 <pbca26@gmail.com>
2016-09-04 05:11:44 -0400

    T480 fixed remaining code style issues

--- webui/js/reference-currency.js
+++ webui/js/reference-currency.js
@@ -1,24 +1,34 @@
-$(document).ready(function(e) { 	 
-  var json_Obj =currencyArr;            
-  var outPut="";
-  var defaultActive="";  
-    for (var i in json_Obj) 
-    { 
-		defaultActive="";
-		if(json_Obj[i].selected==1){defaultActive=" selected"; }
-		outPut+="<li class='country-li "+defaultActive+"' data-id='"
-	   	 +json_Obj[i].id 
-       	 +"'><h1 class='flag-head'><span class='label label-default'><span class='flag-icon flag-icon-"
-	   	 +json_Obj[i].flagid+"'></span></span></h1><strong class='short-name'>" 
-	   	 + json_Obj[i].shortName + "</strong><span class='full-name'> "
-	   	 +json_Obj[i].fullName+"</span></li>"; 
-	}
-	$('.currency-loop').html(outPut);			
-  	$('.country-li').on("click",function(){	
-  		console.clear()
-    	$('.country-li').removeClass("selected");
-    	$(this).addClass("selected");			
-    	var id=$(this).attr("data-id");
-    	console.log(currency_arr[id-1]);
+var currencyArr = [ { "id": 1, "shortName": "USD", "fullName": "United States Dollar", "flagid": "us", "selected" :1 },
+                    { "id": 2, "shortName": "EUR", "fullName": "Euro", "flagid": "eu", "selected": 0 }];
+
+
+$(document).ready(function(e) {
+  var outPut = "";
+  var defaultActive = "";
+
+  for (var i in currencyArr)
+  {
+	  defaultActive = "";
+
+    if (currencyArr[i].selected == 1) {
+      defaultActive = "selected";
+    }
+
+    outPut += "<li class=\"country-li " + defaultActive + "\" data-id=\"" + currencyArr[i].id + "\">" +
+                "<h1 class=\"flag-head\">" +
+                  "<span class=\"label label-default\">" +
+                    "<span class=\"flag-icon flag-icon-" + currencyArr[i].flagid + "\"></span>" +
+                  "</span>" +
+                "</h1>" +
+                "<strong class=\"short-name\">" + currencyArr[i].shortName + "</strong>" +
+                "<span class=\"full-name\">" + currencyArr[i].fullName + "</span>" +
+              "</li>";
+  }
+  $('.currency-loop').html(outPut);
+  	$('.country-li').on("click",function(){
+      var id = $(this).attr("data-id");
+
+      $('.country-li').removeClass("selected");
+    	$(this).addClass("selected");
 	});
 });
