868143fa1d798f71f1fcd58eca8eae6796f22926
jl777 <jameslee777@yahoo.com>
2016-09-10 06:29:57 -0300

    test

--- iguana/iguana_bundles.c
+++ iguana/iguana_bundles.c
@@ -1498,9 +1498,9 @@ void iguana_bundlestats(struct supernet_info *myinfo,struct iguana_info *coin,ch
                             iguana_blockQ("stuck",coin,bp,i,block->RO.hash2,1);
                     }
                 }
-                //for (i=0; i<bp->n; i++)
-                //    if ( GETBIT(bp->haveblock,i) == 0 )
-                //        bp->issued[i] = 0;
+                for (i=0; i<bp->n; i++)
+                    if ( GETBIT(bp->haveblock,i) == 0 )
+                        bp->issued[i] = 0;
                 if (  (n= iguana_bundleissuemissing(myinfo,coin,bp,3,1.)) > 0 )
                     printf("issued %d priority requests [%d] to unstick stuckiters.%d lag.%d\n",n,bp->hdrsi,coin->stuckiters,lag);
                 //else printf("no bundlerequests issued\n");
