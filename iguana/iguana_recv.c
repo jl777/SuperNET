e6a0c91108491677de828ae57b3937927de9f58f
jl777 <jameslee777@yahoo.com>
2016-09-11 11:49:21 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -2033,7 +2033,7 @@ int32_t iguana_blockQ(char *argstr,struct iguana_info *coin,struct iguana_bundle
         {
             if ( bp->emitfinish != 0 )
                 return(0);
-            if ( bp->issued[bundlei] > 0 )
+            if ( coin->RTheight == 0 && bp->issued[bundlei] > 0 )
                 return(0);
         }
         if ( priority != 0 )
