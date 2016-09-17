3b7ffae825942fae2232bb5b234873bc8bbce777
jl777 <jameslee777@yahoo.com>
2016-09-17 17:23:57 -0300

    test

--- iguana/iguana_peers.c
+++ iguana/iguana_peers.c
@@ -449,10 +449,10 @@ int32_t iguana_socket(int32_t bindflag,char *hostname,uint16_t port)
     }
     timeout.tv_sec = 0;
     timeout.tv_usec = 30000;
-    setsockopt(sock,SOL_SOCKET,SO_RCVTIMEO,(void *)&timeout,sizeof(timeout));
+    //setsockopt(sock,SOL_SOCKET,SO_RCVTIMEO,(void *)&timeout,sizeof(timeout));
     timeout.tv_sec = 0;
     timeout.tv_usec = 10000;
-    setsockopt(sock,SOL_SOCKET,SO_SNDTIMEO,(void *)&timeout,sizeof(timeout));
+    //setsockopt(sock,SOL_SOCKET,SO_SNDTIMEO,(void *)&timeout,sizeof(timeout));
     return(sock);
 }
 
