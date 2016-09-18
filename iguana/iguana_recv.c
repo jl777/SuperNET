fa32d47578b83ffe88bc6aba6838d4e3f63f31e7
jl777 <jameslee777@yahoo.com>
2016-09-18 15:59:18 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -106,7 +106,7 @@ int32_t iguana_sendblockreqPT(struct iguana_info *coin,struct iguana_peer *addr,
         if ( (rand() % 10000) == 0 )
             printf("iguana_sendblockreq (%s) addrind.%d hasn't verack'ed yet\n",addr->ipaddr,addr->addrind);
         //iguana_send_version(coin,addr,coin->myservices);
-        return(-1);
+        //return(-1);
     }
     lastreq2 = lastreq;
     lastreq = hash2;
