eb45b87908f6fc75aa37964981470aaa0f120037
jl777 <jameslee777@yahoo.com>
2016-09-20 14:50:20 -0300

    test

--- iguana/iguana_spendvectors.c
+++ iguana/iguana_spendvectors.c
@@ -706,7 +706,7 @@ int32_t iguana_volatilesinit(struct supernet_info *myinfo,struct iguana_info *co
                 if ( fwrite(&crc,1,sizeof(crc),fp) != sizeof(crc) || fwrite(&balancehash,1,sizeof(balancehash),fp) != sizeof(balancehash) || fwrite(&allbundles,1,sizeof(allbundles),fp) != sizeof(allbundles) )
                     printf("error writing.(%s)\n",crcfname);
                 fclose(fp);
-                if ( coin->longestchain-coin->chain->minconfirms < coin->bundlescount*coin->chain->bundlesize )
+                if ( coin->longestchain <= coin->bundlescount*coin->chain->bundlesize-coin->chain->minconfirms )
                 {
                     for (i=0; i<coin->bundlescount-1; i++)
                     {
