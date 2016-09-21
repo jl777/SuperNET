48bf58e5e1ccbfc10204010f6fe6daf8c9cde308
jl777 <jameslee777@yahoo.com>
2016-09-21 08:08:01 -0300

    test

--- iguana/iguana_spendvectors.c
+++ iguana/iguana_spendvectors.c
@@ -715,7 +715,7 @@ int32_t iguana_volatilesinit(struct supernet_info *myinfo,struct iguana_info *co
                 fclose(fp);
                 //if ( strcmp("BTC",coin->symbol) == 0 )
                 {
-                    if ( (coin->longestchain+coin->chain->minconfirms)/coin->chain->bundlesize < coin->bundlescount*coin->chain->bundlesize )
+                    if ( (coin->longestchain-coin->chain->minconfirms)/coin->chain->bundlesize < coin->bundlescount*coin->chain->bundlesize )
                     {
                         for (i=0; i<coin->bundlescount-1; i++)
                         {
