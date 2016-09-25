3f84c877b7c7a1d62d9d7283502aeaccada4cbc9
jl777 <jameslee777@yahoo.com>
2016-09-25 18:28:09 -0300

    test

--- basilisk/basilisk_swap.c
+++ basilisk/basilisk_swap.c
@@ -331,7 +331,7 @@ int32_t basilisk_swapuserdata(uint8_t *userdata,int32_t pushpriv,bits256 privkey
     {
         userdata[len++] = sizeof(privkey);
         for (i=0; i<sizeof(privkey); i++)
-            userdata[len++] = privkey.bytes[sizeof(privkey) - 1 - i];
+            userdata[len++] = privkey.bytes[i];//sizeof(privkey) - 1 - i];
     }
     userdata[len++] = 0x51 * ifpath; // ifpath == 1 -> if path, 0 -> else path
     return(len);
