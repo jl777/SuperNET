a7ce5162cc05dd21b207707d6385cd1ea4362fce
jl777 <jameslee777@yahoo.com>
2016-09-25 20:15:32 -0300

    test

--- iguana/iguana_interpreter.c
+++ iguana/iguana_interpreter.c
@@ -1353,11 +1353,11 @@ int32_t bitcoin_assembler(struct iguana_info *coin,cJSON *logarray,uint8_t scrip
                             calc_rmd160_sha256(revrmd160,databuf,datalen);
                             for (i=0; i<20; i++)
                             {
-                                rmd160[i] = i;//revrmd160[19-i];
+                                rmd160[i] = revrmd160[19-i];
                                 printf("%02x",revrmd160[i]);
                             }
                             printf(" <- rev OP_HASH160\n");
-                            iguana_pushdata(stacks,0,rmd160,sizeof(rmd160));
+                            iguana_pushdata(stacks,0,revrmd160,sizeof(rmd160));
                             break;
                         case IGUANA_OP_SHA256:
                             vcalc_sha256(0,hash.bytes,databuf,datalen);
