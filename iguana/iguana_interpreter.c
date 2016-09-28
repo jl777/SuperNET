d7d8e8e0d3efe46f1344c28ccf64d0b55afff3fa
jl777 <jameslee777@yahoo.com>
2016-09-28 10:02:13 -0300

    test

--- iguana/iguana_interpreter.c
+++ iguana/iguana_interpreter.c
@@ -1366,7 +1366,24 @@ int32_t bitcoin_assembler(struct iguana_info *coin,cJSON *logarray,uint8_t scrip
                                 revcalc_rmd160_sha256(rmd160,*(bits256 *)databuf);
                                 printf("SPECIAL CASE REVERSE\n");
                             } else*/
+                            for (i=0; i<32; i++)
+                                printf("%02x",databuf[i]);
+                            printf(" <- databuf\n");
+                            for (i=0; i<32; i++)
+                                printf("%02x",revdatabuf[i]);
+                            printf(" <- revdatabuf\n");
+                            calc_rmd160_sha256(rmd160,revdatabuf,datalen);
+                            for (i=0; i<20; i++)
+                                printf("%02x",rmd160[i]);
+                            printf(" <- rmd160 revdatabuf\n");
+                            revcalc_rmd160_sha256(rmd160,*(bits256 *)databuf);
+                            for (i=0; i<20; i++)
+                                printf("%02x",rmd160[i]);
+                            printf(" <- rmd160 special\n");
                             calc_rmd160_sha256(rmd160,databuf,datalen);
+                            for (i=0; i<20; i++)
+                                printf("%02x",rmd160[i]);
+                            printf(" <- rmd160 databuf\n");
                             iguana_pushdata(stacks,0,rmd160,sizeof(rmd160));
                             break;
                         case IGUANA_OP_SHA256:
