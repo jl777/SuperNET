7b262e4c0c4e950af8ad8ae85626eac713bd7af0
jl777 <jameslee777@yahoo.com>
2016-10-10 09:13:10 -0300

    test

--- iguana/iguana_notary.c
+++ iguana/iguana_notary.c
@@ -770,7 +770,7 @@ void dpow_txidupdate(struct supernet_info *myinfo,struct dpow_info *dp,struct ig
         //    continue;
         for (j=0; j<sizeof(desthash); j++)
             desthash.bytes[j] = notaries[i].pubkey[j+1];
-        if ( (retarray= basilisk_channelget(myinfo,desthash,srchash,channel,height,0)) != 0 )
+        if ( (retarray= basilisk_channelget(myinfo,srchash,desthash,channel,height,0)) != 0 )
         {
             //printf("TXIDUPDATE.(%s)\n",jprint(retarray,0));
             if ( (m= cJSON_GetArraySize(retarray)) != 0 )
