6b15a1a3133c66adff5b7ba2ec184121a10448ed
jl777 <jameslee777@yahoo.com>
2016-10-16 09:06:37 -0300

    test

--- iguana/dpow/dpow_network.c
+++ iguana/dpow/dpow_network.c
@@ -112,7 +112,7 @@ void dpow_nanomsg_update(struct supernet_info *myinfo)
                 {
                     myinfo->DPOW.crcs[firstz] = crc32;
                     printf("NANORECV ht.%d channel.%08x (%d) crc32.%08x:%08x datalen.%d:%d\n",np->height,np->channel,size,np->crc32,crc32,np->datalen,(int32_t)(size - sizeof(*np)));
-                    dpow_datahandler(myinfo,np->channel,np->height,np->packet,np->datalen);
+                    dpow_datahandler(myinfo,np->channel,np->height,np->packet,np->datalen,1);
                 }
             } else printf("np->datalen.%d (size %d - %ld)\n",np->datalen,size,sizeof(*np));
             if ( np != 0 )
