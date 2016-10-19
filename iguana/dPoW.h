c3625adfdad3335ecc9a3a423c18ce13a95850bd
jl777 <jameslee777@yahoo.com>
2016-10-18 21:38:16 -0300

    test

--- iguana/dPoW.h
+++ iguana/dPoW.h
@@ -21,7 +21,7 @@
 #define DPOW_M(bp) ((bp)->minsigs)  // (((bp)->numnotaries >> 1) + 1)
 #define DPOW_MODSTART(bp) (((bp)->height / DPOW_CHECKPOINTFREQ) % (bp)->numnotaries)
 #define DPOW_VERSION 0x0206
-#define DPOW_UTXOSIZE 20000
+#define DPOW_UTXOSIZE 10000
 #define DPOW_MINOUTPUT 6000
 #define DPOW_DURATION 300
 #define DPOW_RATIFYDURATION (3600 * 24)
