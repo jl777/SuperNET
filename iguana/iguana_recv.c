bc18ed7928b26a980e1ad7b7b3f32dcfe5a62d60
jl777 <jameslee777@yahoo.com>
2016-11-03 09:28:24 -0300

    test

--- iguana/iguana_recv.c
+++ iguana/iguana_recv.c
@@ -845,9 +845,9 @@ void iguana_gotblockM(struct supernet_info *myinfo,struct iguana_info *coin,stru
     txdata = origtxdata;
     if ( 1 || iguana_ramchain_data(myinfo,coin,addr,origtxdata,txarray,origtxdata->zblock.RO.txn_count,data,recvlen,bp,block) >= 0 )
     {
-        /*txdata->zblock.fpipbits = (uint32_t)addr->ipbits;
-        txdata->zblock.RO.recvlen = recvlen;
-        txdata->zblock.fpos = 0;
+        //txdata->zblock.fpipbits = (uint32_t)addr->ipbits;
+        //txdata->zblock.RO.recvlen = recvlen;
+        //txdata->zblock.fpos = 0;
         copyflag = (coin->enableCACHE != 0) && (strcmp(coin->symbol,"BTC") != 0);
         req = iguana_recv_bundlereq(coin,addr,copyflag,H,data,recvlen,bp,bundlei,txdata);
         queue_enqueue("recvQ",&coin->recvQ,&req->DL);
@@ -881,7 +881,7 @@ void iguana_gotblockM(struct supernet_info *myinfo,struct iguana_info *coin,stru
                 iguana_bundleQ(myinfo,coin,bp,0);
                 //printf("numsaved.%d [%d] %s\n",numsaved,bp->hdrsi,addr->ipaddr);
             }
-        }*/
+        }
     }
     else
     {
