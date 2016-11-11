b8ad3efcf7b05173660b39cf78e488954288e853
jl777 <jameslee777@yahoo.com>
2016-11-10 21:37:18 -0300

    test

--- iguana/dpow/dpow_fsm.c
+++ iguana/dpow/dpow_fsm.c
@@ -37,7 +37,7 @@ void dpow_ratify_update(struct supernet_info *myinfo,struct dpow_info *dp,struct
                 else bp->ratifysigmasks[i] &= ~(1LL << senderind);
             }
         }
-        bp->ratifyrecvmask |= (1LL << senderind);
+        bp->ratifyrecvmask |= (1LL << senderind) | (1LL << bp->myind);
         bp->ratifybestmask = dpow_ratifybest(bp->ratifyrecvmask,bp,&bp->ratifybestk);
         if ( bp->ratifybestk >= 0 )
         {
