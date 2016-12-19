d184cc81638c28c1f41c63baf8cb4744816da5d6
jl777 <jameslee777@yahoo.com>
2016-12-19 12:53:31 +0200

    test

--- iguana/dpow/dpow_network.c
+++ iguana/dpow/dpow_network.c
@@ -879,7 +879,7 @@ void dpow_nanomsg_update(struct supernet_info *myinfo)
         }
         if ( np != 0 )
             nn_freemsg(np), np = 0;
-        //if ( size == 0 || nun > 10000 )
+        if ( size == 0 || num > 100 )
             break;
     }
     portable_mutex_unlock(&myinfo->dpowmutex);
