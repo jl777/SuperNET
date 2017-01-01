cc9285b27956672d5e8ae0711e1d4319a9867657
jl777 <jameslee777@yahoo.com>
2017-01-01 10:26:00 +0200

    17

--- iguana/dPoW.h
+++ iguana/dPoW.h
@@ -19,7 +19,7 @@
 #define DPOW_FIRSTRATIFY 1000
 
 #define DPOW_CHECKPOINTFREQ 10
-#define DPOW_MINSIGS 11
+#define DPOW_MINSIGS 17
 //#define DPOW_M(bp) ((bp)->minsigs)  // (((bp)->numnotaries >> 1) + 1)
 #define DPOW_MODIND(bp,offset) (((((bp)->height / DPOW_CHECKPOINTFREQ) % (bp)->numnotaries) + (offset)) % (bp)->numnotaries)
 #define DPOW_VERSION 0x0777
