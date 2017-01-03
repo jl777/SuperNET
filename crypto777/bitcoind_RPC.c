9651183dd424dc358e98651a7aab0f8d1bd9ad9e
jl777 <jameslee777@yahoo.com>
2017-01-03 11:44:45 +0200

    test

--- crypto777/bitcoind_RPC.c
+++ crypto777/bitcoind_RPC.c
@@ -89,7 +89,7 @@ char *post_process_bitcoind_RPC(char *debugstr,char *command,char *rpcstr,char *
         }
         else if ( (error->type&0xff) != cJSON_NULL || (result->type&0xff) != cJSON_NULL )
         {
-            if ( strcmp(command,"signrawtransaction") != 0 )
+            if ( strcmp(command,"signrawtransaction") != 0 && strcmp(command,"sendrawtransaction") != 0 )
                 printf("<<<<<<<<<<< bitcoind_RPC: %s post_process_bitcoind_RPC (%s) error.%s\n",debugstr,command,rpcstr);
             retstr = rpcstr;
             rpcstr = 0;
