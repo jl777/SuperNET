602e0e7614ff9778087efc439c4ed8b8c418b219
jl777 <jameslee777@yahoo.com>
2017-01-22 11:53:40 +0200

    test

--- iguana/iguana777.c
+++ iguana/iguana777.c
@@ -901,7 +901,7 @@ void iguana_coinloop(void *arg)
                     if ( coin->notarychain >= 0 && myinfo->IAMNOTARY != 0 )
                         init_alladdresses(myinfo,coin);
                 }
-                if ( coin->FULLNODE < 0 || coin->notarychain >= 0 )
+                if ( coin->FULLNODE < 0 || (coin->notarychain >= 0 && coin->FULLNODE == 0) )
                 {
                     continue;
                 }
@@ -1209,7 +1209,7 @@ int32_t iguana_launchcoin(struct supernet_info *myinfo,char *symbol,cJSON *json,
         coins = mycalloc('A',1+1,sizeof(*coins));
         if ( (coin= iguana_setcoin(symbol,coins,maxpeers,maxrecvcache,services,initialheight,maphash,minconfirms,maxrequests,maxbundles,json,virtcoin)) != 0 )
         {
-            if ( iguana_isnotarychain(coin->symbol) < 0 )
+            if ( iguana_isnotarychain(coin->symbol) < 0 || coin->FULLNODE != 0 )
             {
                 coins[0] = (void *)((long)1);
                 coins[1] = coin;
