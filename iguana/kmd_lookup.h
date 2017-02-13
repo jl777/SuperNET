24aa2d0946ae067545ec041e61a7ce94fcfd5845
jl777 <jameslee777@yahoo.com>
2017-02-13 02:18:24 +0200

    test

--- iguana/kmd_lookup.h
+++ iguana/kmd_lookup.h
@@ -703,7 +703,7 @@ int32_t _kmd_bitcoinscan(struct iguana_info *coin)
             }
             free_json(blockjson);
         }
-        if ( flag != 0 || num > 1000 )
+        if ( flag != 0 || num > 500 )
             break;
         coin->kmd_height = loadheight++;
     }
