da18970c671c6f5676a4f8bdad2f85a83adf3aed
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-03-04 23:32:14 +0200

    Test

--- basilisk/basilisk_MSG.c
+++ basilisk/basilisk_MSG.c
@@ -400,7 +400,10 @@ cJSON *basilisk_channelget(struct supernet_info *myinfo,bits256 srchash,bits256 
     jaddnum(valsobj,"numrequired",1);
     jaddbits256(valsobj,"srchash",srchash);
     jaddbits256(valsobj,"desthash",desthash);
-    if ( (retstr= basilisk_getmessage(myinfo,0,0,0,desthash,valsobj,0)) != 0 )
+    if ( myinfo->IAMNOTARY != 0 )
+        retstr = basilisk_getmessage(myinfo,0,0,0,desthash,valsobj,0);
+    else retstr = _dex_getmessage(myinfo,jprint(valsobj,0));
+    if ( retstr != 0 )
     {
         printf("channel.%u msgid.%u gotmessage.(%d) %s\n",channel,msgid,(int32_t)strlen(retstr),strlen(retstr) < 100 ? retstr : "(too long)");
         if ( (retarray= cJSON_Parse(retstr)) != 0 )
