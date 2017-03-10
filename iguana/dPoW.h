3c61c0668fc3a2700201a6d1b51a962d192d484a
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-03-10 10:10:53 +0200

    Test

--- iguana/dPoW.h
+++ iguana/dPoW.h
@@ -23,7 +23,7 @@
 //#define DPOW_M(bp) ((bp)->minsigs)  // (((bp)->numnotaries >> 1) + 1)
 #define DPOW_MODIND(bp,offset) (((((bp)->height / DPOW_CHECKPOINTFREQ) % (bp)->numnotaries) + (offset)) % (bp)->numnotaries)
 #define DPOW_VERSION 0x0781
-#define DPOW_UTXOSIZE 10000
+#define DPOW_UTXOSIZE 30000
 #define DPOW_MINOUTPUT 6000
 #define DPOW_DURATION 600
 #define DPOW_RATIFYDURATION (3600 * 24)
