72a288eb8396902aa77e18a3011bb33c4244296d
jl777 <jameslee777@yahoo.com>
2017-03-25 12:55:40 +0200

    batch14

--- iguana/main.c
+++ iguana/main.c
@@ -2055,7 +2055,7 @@ void komodo_ICO_batch(cJSON *array,int32_t batchid)
     if ( (n= cJSON_GetArraySize(array)) > 0 )
     {
         totalKMD = totalREVS = 0;
-        for (iter=0; iter<1; iter++)
+        for (iter=3; iter<4; iter++)
         for (i=0; i<n; i++)
         {
             item = jitem(array,i);
@@ -2155,7 +2155,7 @@ void iguana_main(void *arg)
     else printf("ENDIAN ERROR\n");
     mycalloc(0,0,0);
 #ifdef __APPLE__
-    char *batchstr,*batchstr2; cJSON *batchjson; long batchsize; char fname[512],fname2[512]; int32_t batchid = 14;
+    char *batchstr,*batchstr2; cJSON *batchjson; long batchsize; char fname[512],fname2[512]; int32_t batchid = 15;
     sprintf(fname,"REVS.raw"), sprintf(fname2,"REVS.rawtxids");
     if ( (0) && (batchstr= OS_filestr(&batchsize,fname)) != 0 && (batchstr2= OS_filestr(&batchsize,fname2)) != 0 )
     {
