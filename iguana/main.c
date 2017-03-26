f3dbb8e68b2305145586c3246ef301ac323f31cd
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-03-26 22:53:01 +0300

    port= arg

--- iguana/main.c
+++ iguana/main.c
@@ -2192,6 +2192,11 @@ void iguana_main(void *arg)
             myinfo->IAMNOTARY = 1;
             myinfo->DEXEXPLORER = 1;
         }
+        else if ( strncmp((char *)arg,"port=",5) == 0 )
+        {
+            myinfo->rpcport = atoi(&((char *)arg)[5]);
+            printf("OVERRIDE IGUANA port <- %u\n",myinfo->rpcport);
+        }
     }
 #ifdef IGUANA_OSTESTS
     do_OStests = 1;
