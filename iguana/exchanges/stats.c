3f1ddbd43b30aaf38142240840121bc59875d755
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-05-02 21:02:54 +0300

    Test

--- iguana/exchanges/stats.c
+++ iguana/exchanges/stats.c
@@ -92,9 +92,9 @@ void komodo_kvupdate(int32_t ht,bits256 txid,int32_t vout,uint8_t *opretbuf,int3
         for (i=0; i<keylen; i++)
             putchar((char)key[i]);
         printf(" -> ");
-        for (i=0; i<coresize; i++)
-            printf("%c",(char)valueptr[i]);
-        decode_hex(decodestr,coresize,valueptr);
+        //for (i=0; i<coresize; i++)
+        //    printf("%c",(char)valueptr[i]);
+        init_hexbytes_noT(decodestr,valueptr,coresize);
         char str[65]; printf(" (%s) [%d] %s/v%d ht.%d height.%d\n",(char *)valueptr,valuesize,bits256_str(str,txid),vout,ht,height);
     }
 }
