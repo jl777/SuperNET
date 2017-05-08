1befacf084d863289715796513d352b16247fa7f
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-05-08 10:16:40 +0300

    Test

--- iguana/exchanges/DEXstats.h
+++ iguana/exchanges/DEXstats.h
@@ -771,9 +771,9 @@ struct DEXstats_priceinfo *stats_prices(char *symbol,char *dest,struct DEXstats_
         if ( strcmp(Prices[i].symbol,symbol) == 0 )
         {
             pp = &Prices[i];
-            if ( leftdatenum >= pp->firstdatenum && leftdatenum < pp->firstdatenum+pp->numdates )
+            if ( leftdatenum+numdates >= pp->firstdatenum && leftdatenum <= pp->firstdatenum+pp->numdates )
             {
-                printf(" (%s) numdates %d 1st %d left %d\n",symbol,pp->numdates,pp->firstdatenum,leftdatenum);
+                printf(" (%s) numdates %d 1st %d left %d+%d\n",symbol,pp->numdates,pp->firstdatenum,leftdatenum,numdates);
                 for (j=0; j<pp->numdates; j++)
                 {
                     datenum = pp->firstdatenum+j;
