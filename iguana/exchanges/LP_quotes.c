7552abb8757f0efc73aac8417a7fe7912f5aa251
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-05 14:21:38 +0300

    Test

--- iguana/exchanges/LP_quotes.c
+++ iguana/exchanges/LP_quotes.c
@@ -249,7 +249,7 @@ cJSON *LP_autotrade(struct LP_utxoinfo *myutxo,char *base,double maxprice)
         maxprice = LP_price(base,myutxo->coin) / 0.975;
     if ( (array= LP_tradecandidates(myutxo,base)) != 0 )
     {
-        printf("candidates.(%s)\n",jprint(array,0));
+        printf("candidates.(%s)\nn.%d",jprint(array,0),cJSON_GetArraySize(array));
         if ( (n= cJSON_GetArraySize(array)) > 0 )
         {
             memset(prices,0,sizeof(prices));
