0eae00f34567445b81255f222650607f0b473059
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-05 14:49:36 +0300

    Test

--- iguana/exchanges/LP_commands.c
+++ iguana/exchanges/LP_commands.c
@@ -300,7 +300,7 @@ char *stats_JSON(cJSON *argjson,char *remoteaddr,uint16_t port) // from rpc port
     else if ( IAMCLIENT == 0 && strcmp(method,"getutxos") == 0 && (coin= jstr(argjson,"coin")) != 0 )
     {
         retstr = LP_utxos(LP_mypeer,coin,jint(argjson,"lastn"));
-        printf("RETURN.(%s)\n",retstr);
+        //printf("RETURN.(%s)\n",retstr);
     }
     else if ( IAMCLIENT == 0 && strcmp(method,"notify") == 0 )
         retstr = clonestr("{\"result\":\"success\",\"notify\":\"received\"}");
