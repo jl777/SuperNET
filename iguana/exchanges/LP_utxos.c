e117b2ad4ec5cc10c394bcc8cedf9458b915d398
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-06 20:18:21 +0300

    Test

--- iguana/exchanges/LP_utxos.c
+++ iguana/exchanges/LP_utxos.c
@@ -52,7 +52,7 @@ struct LP_utxoinfo *_LP_utxo2find(bits256 txid,int32_t vout)
     struct LP_utxoinfo *utxo=0; uint8_t key[sizeof(txid) + sizeof(vout)];
     memcpy(key,txid.bytes,sizeof(txid));
     memcpy(&key[sizeof(txid)],&vout,sizeof(vout));
-    HASH_FIND(hh,LP_utxoinfos2,key,sizeof(key),utxo);
+    //HASH_FIND(hh,LP_utxoinfos2,key,sizeof(key),utxo);
     return(utxo);
 }
 
@@ -61,9 +61,9 @@ struct LP_utxoinfo *LP_utxo2find(bits256 txid,int32_t vout)
     struct LP_utxoinfo *utxo=0; uint8_t key[sizeof(txid) + sizeof(vout)];
     memcpy(key,txid.bytes,sizeof(txid));
     memcpy(&key[sizeof(txid)],&vout,sizeof(vout));
-    portable_mutex_lock(&LP_utxomutex);
+    //portable_mutex_lock(&LP_utxomutex);
     utxo = _LP_utxo2find(txid,vout);
-    portable_mutex_unlock(&LP_utxomutex);
+    //portable_mutex_unlock(&LP_utxomutex);
     return(utxo);
 }
 
@@ -240,8 +240,8 @@ struct LP_utxoinfo *LP_addutxo(int32_t amclient,struct LP_peerinfo *mypeer,int32
         char str[65],str2[65]; printf("amclient.%d %s:%u %s LP_addutxo.(%.8f %.8f) numutxos.%d %s %s\n",IAMCLIENT,ipaddr,port,utxo->coin,dstr(value),dstr(value2),mypeer!=0?mypeer->numutxos:0,bits256_str(str,utxo->txid),bits256_str(str2,txid2));
         portable_mutex_lock(&LP_utxomutex);
         HASH_ADD_KEYPTR(hh,LP_utxoinfos,utxo->key,sizeof(utxo->key),utxo);
-        if ( _LP_utxo2find(txid2,vout2) == 0 )
-            HASH_ADD_KEYPTR(hh,LP_utxoinfos2,utxo->key2,sizeof(utxo->key2),utxo);
+        //if ( _LP_utxo2find(txid2,vout2) == 0 )
+        //    HASH_ADD_KEYPTR(hh,LP_utxoinfos2,utxo->key2,sizeof(utxo->key2),utxo);
         if ( mypeer != 0 )
             mypeer->numutxos++;
         portable_mutex_unlock(&LP_utxomutex);
