362c694d2c36c2a644a57aa4e97b028e4e96bfd7
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-11 13:49:24 +0300

    Test

--- iguana/exchanges/LP_utxos.c
+++ iguana/exchanges/LP_utxos.c
@@ -508,7 +508,7 @@ cJSON *LP_inventory(char *symbol,int32_t iambob)
     else myipaddr = "127.0.0.1";
     HASH_ITER(hh,LP_utxoinfos[iambob],utxo,tmp)
     {
-        char str[65]; printf("iambob.%d iterate %s\n",iambob,bits256_str(str,utxo->payment.txid));
+        char str[65]; printf("iambob.%d iterate %s\n",iambob,bits256_str(str,LP_mypubkey));
         if ( LP_isunspent(utxo) != 0 && strcmp(symbol,utxo->coin) == 0 && utxo->iambob == iambob && LP_ismine(utxo) > 0 )
             jaddi(array,LP_inventoryjson(cJSON_CreateObject(),utxo));
         else printf("skip %s %d %d %d %d\n",bits256_str(str,utxo->pubkey),LP_isunspent(utxo) != 0,strcmp(symbol,utxo->coin) == 0,utxo->iambob == iambob,LP_ismine(utxo) > 0);
