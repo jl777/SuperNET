aab87b6c446b1732c95edefaff86bf8ac3be36ab
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-14 17:19:56 +0300

    Test

--- iguana/exchanges/LP_utxos.c
+++ iguana/exchanges/LP_utxos.c
@@ -262,6 +262,7 @@ char *LP_utxos(int32_t iambob,struct LP_peerinfo *mypeer,char *symbol,int32_t la
     printf("LP_utxos iambob.%d symbol.%s firsti.%d lastn.%d\n",iambob,symbol==0?"":symbol,firsti,lastn);
     HASH_ITER(hh,LP_utxoinfos[iambob],utxo,tmp)
     {
+        char str[65]; printf("check %s.%s\n",utxo->coin,bits256_str(str,utxo->payment.txid));
         if ( i++ < firsti )
             continue;
         if ( (symbol == 0 || symbol[0] == 0 || strcmp(symbol,utxo->coin) == 0) && utxo->T.spentflag == 0 )
