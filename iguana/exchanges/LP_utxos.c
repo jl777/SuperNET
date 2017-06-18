feb5ee76fccb06eb28b0a820fd631040334da7e6
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-18 10:37:28 +0300

    Test

--- iguana/exchanges/LP_utxos.c
+++ iguana/exchanges/LP_utxos.c
@@ -381,7 +381,7 @@ int32_t LP_iseligible(uint64_t *valp,uint64_t *val2p,int32_t iambob,char *symbol
         threshold = (iambob != 0) ? LP_DEPOSITSATOSHIS(satoshis) : LP_DEXFEE(satoshis);
         if ( (val2= LP_txvalue(destaddr2,symbol,txid2,vout2)) >= threshold )
         {
-            if ( strcmp(destaddr,destaddr2) == 0 )
+            if ( strcmp(destaddr,destaddr2) != 0 )
                 printf("mismatched %s destaddr %s vs %s\n",symbol,destaddr,destaddr2);
             else if ( (iambob == 0 && val2 > val) || (iambob != 0 && val2 < val) )
                 printf("ineligible due to offsides: val %.8f and val2 %.8f vs %.8f\n",dstr(val),dstr(val2),dstr(satoshis));
