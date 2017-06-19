4c45b3774979e1bf5cba3439a45d8c0fc0fbf5b5
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-19 11:56:55 +0300

    Test

--- iguana/exchanges/LP_nativeDEX.c
+++ iguana/exchanges/LP_nativeDEX.c
@@ -358,7 +358,7 @@ void LP_mainloop(char *myipaddr,struct LP_peerinfo *mypeer,uint16_t mypubport,in
 
 void nn_tests(int32_t pullsock,char *pushaddr)
 {
-    int32_t sock,n,timeout,m=0; char msg[512]; void *ptr;
+    int32_t sock,n,timeout,m=0; char msg[512]; //void *ptr;
     if ( (sock= nn_socket(AF_SP,LP_COMMAND_SENDSOCK)) >= 0 )
     {
         if ( nn_connect(sock,pushaddr) < 0 )
@@ -373,8 +373,8 @@ void nn_tests(int32_t pullsock,char *pushaddr)
             sprintf(msg,"{\"nn_tests\":\"%s\"}",pushaddr);
             n = LP_send(sock,msg,0);//
             sleep(1);
-            m = nn_recv(pullsock,&ptr,NN_MSG,0);
-            //LP_pullsock_check("127.0.0.1",-1,pullsock,0.);
+            //m = nn_recv(pullsock,&ptr,NN_MSG,0);
+            LP_pullsock_check("127.0.0.1",-1,pullsock,0.);
             printf(">>>>>>>>>>>>>>>>>>>>>> sent %d bytes, recv.%d\n",n,m);
         }
     }
