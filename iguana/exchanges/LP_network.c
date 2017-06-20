a6b6387069745ac057c88d194cbf265b46b376cf
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-06-20 15:03:54 +0300

    Test

--- iguana/exchanges/LP_network.c
+++ iguana/exchanges/LP_network.c
@@ -210,7 +210,7 @@ void LP_psockloop(void *_ptr)
                     if ( i < Numpsocks )
                     {
                         ptr = &PSOCKS[i];
-                        if ( now > ptr->lasttime+PSOCK_KEEPALIVE*2 )
+                        if ( now > ptr->lasttime+PSOCK_KEEPALIVE*10 )
                         {
                             printf("PSOCKS[%d] of %d (%u %u) lag.%d IDLETIMEOUT\n",i,Numpsocks,ptr->publicport,ptr->sendport,now - ptr->lasttime);
                             if ( ptr->publicsock >= 0 )
