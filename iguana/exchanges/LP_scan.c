a8750c6acbad6e101581aabc8fea483002862208
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-07-02 16:56:15 +0300

    Test

--- iguana/exchanges/LP_scan.c
+++ iguana/exchanges/LP_scan.c
@@ -59,7 +59,7 @@ int32_t LP_txheight(uint32_t *timestampp,uint32_t *blocktimep,struct iguana_info
     {
         height = jint(blockobj,"height");
         free_json(blockobj);
-    }
+    } else printf("LP_txheight error (%s)\n",jprint(txobj,0));
     return(height);
 }
 
