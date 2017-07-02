e356e339b03957785429825dcba354c3cf1c9646
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-07-02 16:49:03 +0300

    Test

--- iguana/exchanges/LP_nativeDEX.c
+++ iguana/exchanges/LP_nativeDEX.c
@@ -336,6 +336,7 @@ int32_t LP_mainloop_iter(void *ctx,char *myipaddr,struct LP_peerinfo *mypeer,int
     HASH_ITER(hh,LP_coins,coin,ctmp) // firstrefht,firstscanht,lastscanht
     {
         cJSON *obj; int32_t height; bits256 zero;
+        printf("%s ref.%d scan.%d to %d, longest.%d\n",coin->symbol,coin->firstrefht,coin->firstscanht,coin->lastscanht,coin->longestchain);
         memset(zero.bytes,0,sizeof(zero));
         if ( time(NULL) > coin->lastgetinfo+LP_GETINFO_INCR )
         {
@@ -345,9 +346,9 @@ int32_t LP_mainloop_iter(void *ctx,char *myipaddr,struct LP_peerinfo *mypeer,int
                 {
                     coin->longestchain = height;
                     printf(">>>>>>>>>> set %s longestchain %d (ref.%d [%d, %d])\n",coin->symbol,height,coin->firstrefht,coin->firstscanht,coin->lastscanht);
-                }
+                } else printf("cant find blocks in (%s)\n",jprint(obj,0));
                 free_json(obj);
-            }
+            } else printf("error getting info.%s\n",coin->symbol);
             coin->lastgetinfo = (uint32_t)time(NULL);
         }
         if ( coin->firstrefht == 0 )
