ec2984e854713ee54f5fffd4f69a9a608ba5e30e
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-07-04 16:00:13 +0300

    Test

--- iguana/exchanges/LP_coins.c
+++ iguana/exchanges/LP_coins.c
@@ -269,7 +269,7 @@ struct iguana_info *LP_coinfind(char *symbol)
                 coin->inactive = 0;
             else if ( strcmp(symbol,"BTC") == 0 )
             {
-                coin->inactive = !IAMLP * (uint32_t)time(NULL);
+                coin->inactive = (uint32_t)time(NULL); // * !IAMLP
                 printf("BTC inactive.%u\n",coin->inactive);
             }
         }
