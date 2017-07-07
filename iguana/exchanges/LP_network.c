796174494d1dce0b7a798c64de6df939a8db842e
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-07-07 21:16:24 +0300

    Test

--- iguana/exchanges/LP_network.c
+++ iguana/exchanges/LP_network.c
@@ -134,7 +134,7 @@ void queue_loop(void *ignore)
                 if ( duplicate > 0 )
                 {
                     LP_Qfound++;
-                    if ( (LP_Qenqueued % 10) == 0 )
+                    //if ( (LP_Qenqueued % 10) == 0 )
                         printf("found.%u Q.%d err.%d match.%d\n",ptr->crc32,LP_Qenqueued,LP_Qerrors,LP_Qfound);
                     flag = 1;
                 }
@@ -255,7 +255,7 @@ void LP_broadcast_message(int32_t pubsock,char *base,char *rel,bits256 destpub25
                 msg = (void *)jprint(argjson,0);
                 msglen = (int32_t)strlen((char *)msg) + 1;
                 crc32 = calc_crc32(0,&msg[2],msglen - 2);
-                //printf("CRC32.%u (%s)\n",crc32,(char *)msg);
+                printf("CRC32.%u (%s)\n",crc32,(char *)msg);
                 //jdelete(argjson,"method");
                 //jaddstr(argjson,"method","broadcast");
                 //msg = (void *)jprint(argjson,0);
