0125242b5370c4bee14fd022748d2c4680fbf0dd
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-07-08 14:28:47 +0300

    Test

--- iguana/exchanges/LP_nativeDEX.c
+++ iguana/exchanges/LP_nativeDEX.c
@@ -356,6 +356,7 @@ int32_t LP_mainloop_iter(void *ctx,char *myipaddr,struct LP_peerinfo *mypeer,int
     }
     if ( LP_mypeer != 0 && LP_mypeer->numutxos < mostutxos && mostpeer != 0 )
     {
+        printf("myutxos.%d most.%d %s\n",LP_mypeer->numutxos,mostutxos,mostpeer->ipaddr);
         LP_peer_utxosquery(LP_mypeer,myport,pubsock,mostpeer,now,60);
         LP_peer_pricesquery(mostpeer->ipaddr,mostpeer->port);
 
