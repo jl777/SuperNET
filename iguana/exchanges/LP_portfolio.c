9329db81bdef78b07faa909a61c9544762394d65
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-07-25 12:57:09 +0200

    Test

--- iguana/exchanges/LP_portfolio.c
+++ iguana/exchanges/LP_portfolio.c
@@ -432,6 +432,8 @@ void prices_loop(void *ignore)
                             relvolume = sell->relvolume;
                             for (iter=0; iter<3; iter++)
                             {
+                                if ( relvolume < LP_MIN_TXFEE )
+                                    break;
                                 requestid = quoteid = 0;
                                 if ( LP_utxo_bestfit(sellcoin,SATOSHIDEN * relvolume) != 0 )
                                 {
