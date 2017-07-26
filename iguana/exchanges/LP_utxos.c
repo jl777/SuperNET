60427508900aa9ad8671a813af21b962ca267063
jl777 <mac@MacBook-Pro-Vladimir.local>
2017-07-26 13:02:14 +0200

    Test

--- iguana/exchanges/LP_utxos.c
+++ iguana/exchanges/LP_utxos.c
@@ -835,7 +835,7 @@ uint64_t LP_privkey_init(int32_t mypubsock,struct iguana_info *coin,bits256 mypr
                         satoshis = SATOSHIDEN * jdouble(item,"value");
                     if ( LP_inventory_prevent(iambob,jbits256(item,"txid"),juint(item,"vout")) == 0 && jint(item,"confirmations") > 0 )
                     {
-                        printf("%s\n",jprint(item,0));
+                        //printf("%s\n",jprint(item,0));
                         values[i] = satoshis;
                     } else used++;
                     //printf("%.8f ",dstr(satoshis));
