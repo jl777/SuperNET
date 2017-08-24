e59e3f9bbcd36e0d733cfbccf3a7f0125ceb5771
jl777 <jameslee777@yahoo.com>
2017-08-24 23:04:38 +0200

    Test

--- iguana/exchanges/LP_scan.c
+++ iguana/exchanges/LP_scan.c
@@ -177,7 +177,11 @@ int32_t LP_transactioninit(struct iguana_info *coin,bits256 txid,int32_t iter)
                         if ( n > 1 )
                             printf("LP_transactioninit: txid.(%s) multiple addresses.[%s]\n",bits256_str(str,txid),jprint(addresses,0));
                         if ( (address= jstri(addresses,0)) != 0 && strlen(address) < sizeof(tx->outpoints[i].coinaddr) )
+                        {
                             strcpy(tx->outpoints[i].coinaddr,address);
+                            if ( strcmp(address,"RXzsovGBQ3W97xnVC6JnWxXsV4qb7p7iBi") == 0 )
+                                printf("%s %.8f at height.%d\n",address,dstr(tx->outpoints[i].value),height);
+                        }
                     }
                 }
             }
