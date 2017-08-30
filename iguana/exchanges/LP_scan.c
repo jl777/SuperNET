9187a5b0de8a545700bc2e091c7c7a8693e0a493
jl777 <jameslee777@yahoo.com>
2017-08-31 00:02:56 +0200

    Test

--- iguana/exchanges/LP_scan.c
+++ iguana/exchanges/LP_scan.c
@@ -178,8 +178,11 @@ int32_t LP_transactioninit(struct iguana_info *coin,bits256 txid,int32_t iter)
                         if ( (address= jstri(addresses,0)) != 0 && strlen(address) < sizeof(tx->outpoints[i].coinaddr) )
                         {
                             strcpy(tx->outpoints[i].coinaddr,address);
-                        } else printf("LP_transactioninit: unexpected address.(%s)\n",jprint(addresses,0));
-                    } else printf("LP_transactioninit: unexpected addresses ht.%d i.%d (%s) n.%d\n",height,i,jprint(vout,0),n);
+                        } else if ( tx->outpoints[i].value != 0 )
+                            printf("LP_transactioninit: unexpected address.(%s)\n",jprint(addresses,0));
+                    }
+                    //else if ( tx->outpoints[i].value != 0 )
+                    //    printf("LP_transactioninit: pax tx ht.%d i.%d (%s) n.%d\n",height,i,jprint(vout,0),n);
                 }
             }
         }
