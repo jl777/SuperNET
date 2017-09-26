e9113f5897a07c23d74cda079ffe314e7e871288
jl777 <jameslee777@yahoo.com>
2017-09-26 21:11:48 +0200

    Test

--- iguana/exchanges/LP_ordermatch.c
+++ iguana/exchanges/LP_ordermatch.c
@@ -26,11 +26,11 @@ uint64_t LP_txfeecalc(struct iguana_info *coin,uint64_t txfee)
         if ( strcmp(coin->symbol,"BTC") == 0 )
         {
             coin->rate = LP_getestimatedrate(coin);
-            if ( (txfee= coin->rate * LP_AVETXSIZE) < LP_MIN_TXFEE )
+            if ( (txfee= coin->rate * LP_AVETXSIZE) <= LP_MIN_TXFEE )
             {
                 coin->rate = 0.;
                 coin->rate = _LP_getestimatedrate(coin);
-                if ( (txfee= coin->rate * LP_AVETXSIZE) < LP_MIN_TXFEE )
+                if ( (txfee= coin->rate * LP_AVETXSIZE) <= LP_MIN_TXFEE )
                     txfee = LP_MIN_TXFEE;
             }
         } else txfee = coin->txfee;
