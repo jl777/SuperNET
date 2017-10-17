93f815c15cb14df11f1f34ceeaab7df110eb3041
jl777 <jameslee777@yahoo.com>
2017-10-17 16:43:02 +0300

    Test

--- iguana/exchanges/LP_include.h
+++ iguana/exchanges/LP_include.h
@@ -316,6 +316,7 @@ struct LP_transaction *LP_transactionfind(struct iguana_info *coin,bits256 txid)
 cJSON *LP_transactioninit(struct iguana_info *coin,bits256 txid,int32_t iter,cJSON *txobj);
 int32_t LP_mempoolscan(char *symbol,bits256 searchtxid);
 int32_t LP_txheight(struct iguana_info *coin,bits256 txid);
+int32_t LP_numpeers();
 int32_t LP_address_utxoadd(struct iguana_info *coin,char *coinaddr,bits256 txid,int32_t vout,uint64_t value,int32_t height,int32_t spendheight);
 void LP_smartutxos_push(struct iguana_info *coin);
 cJSON *LP_address_utxos(struct iguana_info *coin,char *coinaddr,int32_t electrumret);
