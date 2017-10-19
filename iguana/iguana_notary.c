21b572bb51c2bf697db747405af44726e21d0305
jl777 <jameslee777@yahoo.com>
2017-10-19 14:42:07 +0300

    Test

--- iguana/iguana_notary.c
+++ iguana/iguana_notary.c
@@ -318,12 +318,12 @@ void iguana_dPoWupdate(struct supernet_info *myinfo,struct dpow_info *dp)
                     dpow_srcupdate(myinfo,dp,dp->lastheight++,blockhash,(uint32_t)time(NULL),blocktime);
                 }
             }
-            else if ( time(NULL) > dp->lastsrcupdate+60 )
+            else if ( time(NULL) > dp->lastsrcupdate+60 || height != dp->lastheight )
             {
+                dp->lastsrcupdate = (uint32_t)time(NULL);
                 dp->lastheight = height;
                 blockhash = dpow_getblockhash(myinfo,src,dp->lastheight);
                 dpow_srcupdate(myinfo,dp,dp->lastheight,blockhash,(uint32_t)time(NULL),blocktime);
-                dp->lastsrcupdate = (uint32_t)time(NULL);
             }
         } //else printf("error getchaintip for %s\n",dp->symbol);
     } else printf("iguana_dPoWupdate missing src.(%s) %p or dest.(%s) %p\n",dp->symbol,src,dp->dest,dest);
