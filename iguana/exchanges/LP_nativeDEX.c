4f37be01575b7d43b59d238f475b8359235f427f
jl777 <jameslee777@yahoo.com>
2017-10-22 14:08:05 +0300

    Test

--- iguana/exchanges/LP_nativeDEX.c
+++ iguana/exchanges/LP_nativeDEX.c
@@ -601,7 +601,7 @@ int32_t LP_mainloop_iter(void *ctx,char *myipaddr,struct LP_peerinfo *mypeer,int
         //LP_getestimatedrate(coin);
         break;
     }
-    if ( (counter % 6000) == 60 )
+    if ( (counter % 100000) == 90000 )
     {
         if ( (retstr= basilisk_swapentry(0,0)) != 0 )
         {
