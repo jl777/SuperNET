c45cc8c615f130cc137c0d7a54f6633291a25bb9
jl777 <jameslee777@yahoo.com>
2017-10-24 23:01:39 +0300

    Test

--- iguana/exchanges/LP_ordermatch.c
+++ iguana/exchanges/LP_ordermatch.c
@@ -611,7 +611,7 @@ int32_t LP_tradecommand(void *ctx,char *myipaddr,int32_t pubsock,cJSON *argjson,
         LP_quoteparse(&Q,argjson);
         LP_requestinit(&Q.R,Q.srchash,Q.desthash,Q.srccoin,Q.satoshis-2*Q.txfee,Q.destcoin,Q.destsatoshis-2*Q.desttxfee,Q.timestamp,Q.quotetime,DEXselector);
         LP_tradecommand_log(argjson);
-        printf("LP_tradecommand: check received method %s aliceid.%u\n",method,(long long)Q.aliceid);
+        printf("LP_tradecommand: check received method %s aliceid.%llx\n",method,(long long)Q.aliceid);
         retval = 1;
         if ( strcmp(method,"reserved") == 0 )
         {
