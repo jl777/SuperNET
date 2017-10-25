c377215d5b7835f4d71b541669e35185343af366
jl777 <jameslee777@yahoo.com>
2017-10-26 01:09:50 +0300

    Test

--- crypto777/nanosrc/nn_config.h
+++ crypto777/nanosrc/nn_config.h
@@ -31,9 +31,9 @@
 #define NN_HAVE_SEMAPHORE 1 // must have
 
 // need one of following 3, listed in order of precedence, used by efd*
-#define NN_HAVE_EVENTFD 1
+//#define NN_HAVE_EVENTFD 1
 //#define NN_HAVE_PIPE 1
-//#define NN_HAVE_SOCKETPAIR 1
+#define NN_HAVE_SOCKETPAIR 1
 
 // need one of following 3, listed in order of precedence, used by poller*
 #define NN_USE_POLL 1
