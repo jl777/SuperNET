3faa099f90e5e22759e34fff25a5d84526a96647
jl777 <jameslee777@yahoo.com>
2017-10-30 12:00:42 +0200

    Test

--- iguana/exchanges/LP_rpc.c
+++ iguana/exchanges/LP_rpc.c
@@ -202,7 +202,7 @@ cJSON *LP_NXT_redeems()
                     msgjson = 0;
                     txnum = assetid = qty = 0;
                     item = jitem(array,i);
-                    //printf("%d: %s\n",i,jprint(item,0));
+                    printf("%d: %s\n",i,jprint(item,0));
                     if ( (recv= jstr(item,"recipientRS")) != 0 && strcmp(recv,"NXT-MRBN-8DFH-PFMK-A4DBM") == 0 && jint(item,"version.AssetTransfer") == 1 )
                     {
                         txnum = j64bits(item,"transaction");
@@ -211,7 +211,7 @@ cJSON *LP_NXT_redeems()
                         msgstr = jstr(item,"message");
                         if ( (attach= jobj(item,"attachment")) != 0 )
                         {
-                            //printf("txnum.%llu (%s)\n",(long long)txnum,jprint(attach,0));
+                            printf("txnum.%llu (%s)\n",(long long)txnum,jprint(attach,0));
                             if ( (msgstr == 0 || msgstr[0] == 0) && jint(attach,"version.PrunablePlainMessage") == 1 )
                             {
                                 method = "getPrunableMessage";
