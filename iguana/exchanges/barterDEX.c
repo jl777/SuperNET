9aaeb7423bb23897858aaf86af0365d8ce90b61f
jl777 <jameslee777@yahoo.com>
2017-10-31 16:19:26 +0200

    Test

--- iguana/exchanges/barterDEX.c
+++ iguana/exchanges/barterDEX.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <memory.h>
 #include <string.h>
 
 char *barterDEX(char *jsonstr)
 {
-char *str = "{\"result\":\"success\"}";
+char *retstr,*str = "{\"result\":\"success\"}";
 printf("barterDEX.(%s)\n",jsonstr);
 retstr = malloc(strlen(str)+1);
 strcpy(retstr,str);
