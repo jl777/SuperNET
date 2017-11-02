f6ee03aa51adbf1f97339f352a556a0526d82443
jl777 <jameslee777@yahoo.com>
2017-11-02 19:42:16 +0200

    Test

--- iguana/exchanges/LP_nativeDEX.c
+++ iguana/exchanges/LP_nativeDEX.c
@@ -690,9 +690,12 @@ void LP_initcoins(void *ctx,int32_t pubsock,cJSON *coins)
         printf("%s ",activecoins[i]);
         LP_coinfind(activecoins[i]);
         LP_priceinfoadd(activecoins[i]);
-        if ( (coin= LP_coinfind(activecoins[i])) != 0 && LP_getheight(coin) <= 0 )
-            coin->inactive = (uint32_t)time(NULL);
-        LP_unspents_load(coin->symbol,coin->smartaddr);
+        if ( (coin= LP_coinfind(activecoins[i])) != 0 )
+        {
+            if ( LP_getheight(coin) <= 0 )
+                coin->inactive = (uint32_t)time(NULL);
+            LP_unspents_load(coin->symbol,coin->smartaddr);
+        }
     }
     if ( (n= cJSON_GetArraySize(coins)) > 0 )
     {
@@ -704,9 +707,12 @@ void LP_initcoins(void *ctx,int32_t pubsock,cJSON *coins)
                 printf("%s ",jstr(item,"coin"));
                 LP_coincreate(item);
                 LP_priceinfoadd(jstr(item,"coin"));
-                if ( (coin= LP_coinfind(symbol)) != 0 && LP_getheight(coin) <= 0 )
-                    coin->inactive = (uint32_t)time(NULL);
-                LP_unspents_load(coin->symbol,coin->smartaddr);
+                if ( (coin= LP_coinfind(symbol)) != 0 )
+                {
+                    if ( LP_getheight(coin) <= 0 )
+                        coin->inactive = (uint32_t)time(NULL);
+                    LP_unspents_load(coin->symbol,coin->smartaddr);
+                }
             }
         }
     }
