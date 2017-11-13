b1d772b8aa619bd997b8356edaeb30c7b41fb75a
jl777 <jameslee777@yahoo.com>
2017-11-13 14:16:09 +0200

    Test

--- iguana/exchanges/LP_nativeDEX.c
+++ iguana/exchanges/LP_nativeDEX.c
@@ -21,7 +21,6 @@
 // alice waiting for bestprice
 // regen inventory
 // previously, it used to show amount, kmd equiv, perc
-// there is still a pending one with `-1 wait for bobpayment bYoNxkfvwQ42Yufry8J5y8BYi6mQxokvW9 numconfs.1 MNZ c0ea4aa808a653222a15122d96692fecf734dbbacfb9a54cb4711306ea0c3cef`, but that tx is already spent including 6 confirmation
 // bot safe to exit?
 //
 // BCH signing
