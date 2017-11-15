48623418bbeafdae3cc01059f872e52811e3a416
jl777 <jameslee777@yahoo.com>
2017-11-15 16:04:05 +0200

    Test

--- iguana/exchanges/LP_commands.c
+++ iguana/exchanges/LP_commands.c
@@ -36,14 +36,11 @@ char *stats_JSON(void *ctx,char *myipaddr,int32_t pubsock,cJSON *argjson,char *r
 {
     char *method,*userpass,*base,*rel,*coin,*retstr = 0; int32_t changed,flag = 0; cJSON *retjson,*reqjson = 0; struct iguana_info *ptr;
 method = jstr(argjson,"method");
-if ( strcmp(method,"uitem") == 0 || strcmp(method,"postutxos") == 0
-    || strcmp(method,"notify") == 0
+if ( //strcmp(method,"uitem") == 0 || strcmp(method,"postutxos") == 0 ||
+    strcmp(method,"notify") == 0
     //|| strcmp(method,"postprice") == 0
     )
     return(clonestr("{}"));
-retstr = jprint(argjson,0);
-printf("stats_JSON(%s)\n",retstr);
-free(retstr), retstr = 0;
     /*if ( (ipaddr= jstr(argjson,"ipaddr")) != 0 && (argport= juint(argjson,"port")) != 0 && (method == 0 || strcmp(method,"electrum") != 0) )
     {
         if ( strcmp(ipaddr,"127.0.0.1") != 0 && argport >= 1000 )
