5e378dfd6d3a074ddf16023db22ab9c3ae1f7d81
jl777 <jameslee777@yahoo.com>
2017-12-31 15:35:36 +0700

    Test

--- iguana/exchanges/mm.c
+++ iguana/exchanges/mm.c
@@ -114,10 +114,10 @@ int main(int argc, const char * argv[])
         uint32_t timestamp; char str[65],wifstr[128]; bits256 privkey; int32_t i;
         timestamp = (uint32_t)time(NULL);
         printf("start hush vanitygen t.%u\n",timestamp);
-        for (i=0; i<100000000; i++)
+        for (i=0; i<1000000000; i++)
         {
             privkey = rand256(0);
-            bitcoin_priv2wif(0x36,wifstr,privkey,0xab);
+            bitcoin_priv2wif(0xab,wifstr,privkey,0x36);
             if ( wifstr[4] == 'h' && wifstr[5] == 'u' && wifstr[6] == 's' )
             {
                 printf("i.%d %s -> wif.%s\n",i,bits256_str(str,privkey),wifstr);
