4aa3d30e5f61a6c5686f92be4d47cc2ebd764446
jl777 <jameslee777@yahoo.com>
2017-12-31 15:03:45 +0700

    Test

--- iguana/exchanges/mm.c
+++ iguana/exchanges/mm.c
@@ -112,6 +112,7 @@ int main(int argc, const char * argv[])
     if ( strstr(argv[0],"hush") != 0 && argv[1] != 0 )
     {
         uint8_t pubkey33[33]; char str[65],wifstr[128],coinaddr[64]; bits256 privkey; int32_t i; void *ctx = bitcoin_ctx();
+        printf("start hush vanitygen\n");
         for (i=0; i<100000000; i++)
         {
             privkey = rand256(0);
@@ -123,6 +124,7 @@ int main(int argc, const char * argv[])
                 break;
             }
         }
+        printf("done hush vanitygen\n");
         exit(0);
     }
     sprintf(dirname,"%s",GLOBAL_DBDIR), OS_ensure_directory(dirname);
