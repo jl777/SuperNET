9e2a36b5ee7e76a4e9900f84cba0d9fc51b86d56
jl777 <jameslee777@yahoo.com>
2018-01-23 23:48:35 +0400

    Coinmarketprice margin fix?

--- iguana/exchanges/LP_portfolio.c
+++ iguana/exchanges/LP_portfolio.c
@@ -604,7 +604,7 @@ void LP_autoprice_iter(void *ctx,struct LP_priceinfo *btcpp)
                 LP_mypriceset(&changed,rel,base,newprice);
                 LP_pricepings(ctx,LP_myipaddr,LP_mypubsock,rel,base,newprice);
                 //printf("%s/%s price %.8f margin %.8f/%.8f newprice %.8f %.8f\n",base,rel,price,buymargin,sellmargin,newprice,(1. / newprice) * (1. + sellmargin));
-                newprice = (1. / newprice) * (1. + sellmargin);
+                newprice = (1. / (newprice * (1. + sellmargin)));
                 if ( LP_autorefs[i].lastask < SMALLVAL )
                     LP_autorefs[i].lastask = newprice;
                 else LP_autorefs[i].lastask = (LP_autorefs[i].lastask * 0.9) + (0.1 *newprice);
