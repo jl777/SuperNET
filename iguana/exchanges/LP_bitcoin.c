9d4a47b5f052fbe8749b42d0a2c9d9237a9d39ec
jl777 <jameslee777@yahoo.com>
2018-01-24 12:42:51 +0400

    Test

--- iguana/exchanges/LP_bitcoin.c
+++ iguana/exchanges/LP_bitcoin.c
@@ -2220,11 +2220,12 @@ int32_t bitcoin_wif2priv(char *symbol,uint8_t wiftaddr,uint8_t *addrtypep,bits25
         {
             memcpy(privkeyp,buf+offset,32);
             if ( len > 32+offset )
-                printf("wif: extra byte %d len.%d vs %d addrtype.%d\n",buf[32+offset],len,32+offset,(wiftaddr == 0) ? buf[0] : buf[1]);
+                printf("wif %s: extra byte %d len.%d vs %d addrtype.%d\n",wifstr,buf[32+offset],len,32+offset,(wiftaddr == 0) ? buf[0] : buf[1]);
+            else printf("%s is for uncompressed\n",wifstr);
         }
         else
         {
-            //printf("wif %s -> buf too short len.%d\n",wifstr,len);
+            printf("wif %s -> buf too short len.%d\n",wifstr,len);
             return(-1);
         }
         ptr = buf;
