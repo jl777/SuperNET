afb09723fe6bcaed6443e3cfdb26179e5d53295d
jl777 <jameslee777@yahoo.com>
2018-01-25 11:05:12 +0400

    Test

--- iguana/exchanges/LP_prices.c
+++ iguana/exchanges/LP_prices.c
@@ -539,7 +539,7 @@ int32_t LP_mypriceset(int32_t *changedp,char *base,char *rel,double price)
             if ( price > (1. / maxprice) )
             {
                 printf("%s/%s price %.8f less than maxprice %.8f, more than %.8f\n",base,rel,price,maxprice,1./maxprice);
-                price = maxprice;
+                price = (1. / maxprice);
             }
         }
         /*else if ( basepp->myprices[relpp->ind] > SMALLVAL )
