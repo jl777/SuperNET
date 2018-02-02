fad29e6d2cc4806ca32f11109bd6ae547cbb57e1
jl777 <jameslee777@yahoo.com>
2018-02-02 20:34:30 +0200

    Test

--- iguana/exchanges/mm.c
+++ iguana/exchanges/mm.c
@@ -231,10 +231,10 @@ int main(int argc, const char * argv[])
                         sprintf(manystrs[num++],"\\\"%s\\\":%0.8f",coinaddr,val);
                         if ( num >= sizeof(manystrs)/sizeof(*manystrs) )
                         {
-                            sprintf(cmd,"fiat/btch sendmany \"\" \"{ ");
+                            sprintf(cmd,"fiat/btch sendmany \"\" \"{");
                             for (i=0; i<num; i++)
-                                sprintf(cmd + strlen(cmd),"%s%c",manystrs[i],i<num-1?',':' ');
-                            strcat(cmd,"}\"");
+                                sprintf(cmd + strlen(cmd),"%s%s",manystrs[i],i<num-1?",":"");
+                            strcat(cmd,"} 0\"");
                             printf("%s\n",cmd);
                             num = 0;
                             memset(manystrs,0,sizeof(manystrs));
@@ -246,9 +246,9 @@ int main(int argc, const char * argv[])
             }
             if ( num > 0 )
             {
-                sprintf(cmd,"fiat/btch sendmany \"\" \"{ ");
+                sprintf(cmd,"fiat/btch sendmany \"\" \"{");
                 for (i=0; i<num; i++)
-                    sprintf(cmd + strlen(cmd),"%s%c",manystrs[i],i<num-1?',':' ');
+                    sprintf(cmd + strlen(cmd),"%s%s",manystrs[i],i<num-1?",":"");
                 strcat(cmd,"}\"");
                 printf("%s\n",cmd);
                 num = 0;
