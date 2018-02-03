4b0da668183d361c97cb39237b843d79d85bc321
jl777 <jameslee777@yahoo.com>
2018-02-03 20:36:53 +0200

    Test

--- iguana/exchanges/LP_ordermatch.c
+++ iguana/exchanges/LP_ordermatch.c
@@ -1220,7 +1220,7 @@ int32_t LP_tradecommand(void *ctx,char *myipaddr,int32_t pubsock,cJSON *argjson,
                 //printf("CONNECTED.(%s)\n",jprint(argjson,0));
                 if ( (proof= jarray(&num,argjson,"proof")) != 0 && num > 0 )
                     Q.othercredits = LP_instantdex_proofcheck(Q.srccoin,Q.coinaddr,proof,num);
-                if ( Qtrades == 0 )
+                if ( 1 || Qtrades == 0 )
                     LP_trades_gotconnected(ctx,&Q,&Q2,jstr(argjson,"pair"));
                 else LP_tradecommandQ(&Q,jstr(argjson,"pair"),LP_CONNECTED);
             }
