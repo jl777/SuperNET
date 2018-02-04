a0539941044aecad3737a5b9ab82bcdb42cec45f
jl777 <jameslee777@yahoo.com>
2018-02-04 17:59:12 +0200

    Test

--- iguana/exchanges/LP_utxo.c
+++ iguana/exchanges/LP_utxo.c
@@ -456,6 +456,9 @@ struct LP_address *LP_address_utxo_reset(struct iguana_info *coin)
         printf("LP_address_utxo_reset: cant find address data\n");
         return(0);
     }
+    if ( time(NULL) < coin->lastresetutxo+30 )
+        return(ap);
+    coin->lastresetutxo = (uint32_t)time(NULL);
     if ( (array= LP_listunspent(coin->symbol,coin->smartaddr,zero,zero)) != 0 )
     {
         printf("clear ap->utxos\n");
