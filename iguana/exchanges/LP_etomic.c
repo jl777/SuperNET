8c569350a5c576a1a9ca6420174f95c28963f65d
jl777 <jameslee777@yahoo.com>
2018-02-07 11:09:21 +0200

    Test

--- iguana/exchanges/LP_etomic.c
+++ iguana/exchanges/LP_etomic.c
@@ -79,7 +79,7 @@ int32_t LP_etomic_priv2pub(uint8_t *pub64,bits256 privkey)
     if ( (pubstr= getPubKeyFromPriv(str)) != 0 )
     {
         printf("priv2pub got %s\n",pubstr);
-        if ( strlen(pubstr) == 128 && pubstr[0] == '0' && pubstr[1] == 'x' )
+        if ( strlen(pubstr) == 130 && pubstr[0] == '0' && pubstr[1] == 'x' )
         {
             decode_hex(pub64,64,pubstr+2);
             retval = 64;
