2e42af7b96dd187936b60c36a7d0228640785e75
jl777 <jameslee777@yahoo.com>
2018-02-07 21:18:31 +0200

    Test

--- iguana/exchanges/LP_nativeDEX.c
+++ iguana/exchanges/LP_nativeDEX.c
@@ -533,7 +533,7 @@ void LP_coinsloop(void *_coins)
         nonz = 0;
         HASH_ITER(hh,LP_coins,coin,ctmp) // firstrefht,firstscanht,lastscanht
         {
-            if ( coins != 0 )
+            /*if ( coins != 0 )
             {
                 if ( coins[0] != 0 )
                 {
@@ -545,7 +545,7 @@ void LP_coinsloop(void *_coins)
                     if ( strcmp("BTC",coin->symbol) == 0 || strcmp("KMD",coin->symbol) == 0 )
                         continue;
                 }
-            }
+            }*/
             if ( strcmp(coin->symbol,"BEER") == 0 )
                 continue;
             if ( coin->smartaddr[0] == 0 )
@@ -1397,7 +1397,7 @@ void LPinit(uint16_t myport,uint16_t mypullport,uint16_t mypubport,uint16_t mybu
         printf("error launching LP_coinsloop for (%s)\n","");
         exit(-1);
     }
-    if ( OS_thread_create(malloc(sizeof(pthread_t)),NULL,(void *)LP_coinsloop,(void *)"BTC") != 0 )
+    /*if ( OS_thread_create(malloc(sizeof(pthread_t)),NULL,(void *)LP_coinsloop,(void *)"BTC") != 0 )
     {
         printf("error launching LP_coinsloop for (%s)\n","BTC");
         exit(-1);
@@ -1406,7 +1406,7 @@ void LPinit(uint16_t myport,uint16_t mypullport,uint16_t mypubport,uint16_t mybu
     {
         printf("error launching LP_coinsloop for (%s)\n","KMD");
         exit(-1);
-    }
+    }*/
     if ( OS_thread_create(malloc(sizeof(pthread_t)),NULL,(void *)LP_pubkeysloop,ctx) != 0 )
     {
         printf("error launching LP_pubkeysloop for ctx.%p\n",ctx);
