db26ce04a53b0f9aa6e820d453bcc9369f863e42
jl777 <jameslee777@yahoo.com>
2018-02-09 14:02:51 +0200

    Test

--- iguana/exchanges/LP_remember.c
+++ iguana/exchanges/LP_remember.c
@@ -327,7 +327,7 @@ bits256 basilisk_swap_spendupdate(int32_t iambob,char *symbol,char *spentaddr,in
                     txids[alicespent] = spendtxid;
                 }
             }
-        } else printf("no spend of %s/v%d detected\n",bits256_str(str,txid),vout);
+        } else printf("no spend of %s/v%d detected\n",bits256_str(str,txid),utxovout);
     } //else printf("utxoind.%d null txid\n",utxoind);
     return(spendtxid);
 }
