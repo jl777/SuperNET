ecd0f69d2fbf9ea7442380daf1eb0b0a16e8bff0
jl777 <jameslee777@yahoo.com>
2018-02-27 17:50:08 +0200

    Test

--- iguana/exchanges/LP_coins.c
+++ iguana/exchanges/LP_coins.c
@@ -376,7 +376,8 @@ uint16_t LP_coininit(struct iguana_info *coin,char *symbol,char *name,char *asse
     if ( assetname != 0 && strcmp(name,assetname) == 0 )
     {
         //printf("%s is assetchain\n",symbol);
-        coin->isassetchain = 1;
+        if ( strcmp(name,"BEER") != 0 && strcmp("PIZZA",name) != 0 )
+            coin->isassetchain = 1;
     }
     if ( strcmp(symbol,"KMD") == 0 || (assetname != 0 && assetname[0] != 0) )
         name2 = 0;
