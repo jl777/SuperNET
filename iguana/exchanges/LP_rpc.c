f97a9cdc4d47e3b7c18e2d2131318a27c68744da
jl777 <jameslee777@yahoo.com>
2018-03-27 23:18:15 +0300

    Test

--- iguana/exchanges/LP_rpc.c
+++ iguana/exchanges/LP_rpc.c
@@ -416,6 +416,7 @@ cJSON *LP_listunspent(char *symbol,char *coinaddr,bits256 reftxid,bits256 reftxi
                 usecache = 0;
             else if ( G.LP_pendingswaps != 0 && time(NULL) > ap->unspenttime+1 )
                 usecache = 0;
+            printf("%s %s usecache.%d iswatched.%d\n",coin->symbol,coinaddr,usecache,LP_address_iswatchonly(symbol,coinaddr));
             if ( usecache != 0 && (retstr= LP_unspents_filestr(symbol,coinaddr)) != 0 )
             {
                 retjson = cJSON_Parse(retstr);
@@ -423,7 +424,7 @@ cJSON *LP_listunspent(char *symbol,char *coinaddr,bits256 reftxid,bits256 reftxi
                 return(retjson);
             }
         }
-        //printf("%s %s usecache.%d iswatched.%d\n",coin->symbol,coinaddr,usecache,LP_address_iswatchonly(symbol,coinaddr));
+        printf("%s %s usecache.%d iswatched.%d\n",coin->symbol,coinaddr,usecache,LP_address_iswatchonly(symbol,coinaddr));
         if ( LP_address_ismine(symbol,coinaddr) > 0 || LP_address_iswatchonly(symbol,coinaddr) > 0 )
         {
             if ( strcmp(symbol,"BTC") == 0 )
