6884032703db06d7144b15130d3bcd71651108bd
jl777 <jameslee777@yahoo.com>
2018-04-30 20:02:27 +0300

    BTC 0.16 support for deprecated validate address

--- iguana/iguana777.c
+++ iguana/iguana777.c
@@ -49,6 +49,8 @@ struct iguana_info *iguana_coinadd(char *symbol,char *name,cJSON *argjson,int32_
             myinfo->allcoins_being_added = 1;
             coin = mycalloc('C',1,sizeof(*coin));
             strcpy(coin->getinfostr,"getinfo");
+            strcpy(coin->validateaddress,"validateaddress");
+            strcpy(coin->estimatefeestr,"estimatefee");
             coin->blockspacesize = IGUANA_MAXPACKETSIZE + 8192;
             coin->blockspace = calloc(1,coin->blockspacesize);
             if ( virtcoin != 0 || ((privatechain= jstr(argjson,"geckochain")) != 0 && privatechain[0] != 0) )
