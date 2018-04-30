fa1c79f5a5aceb558612d52a63cf499cbad62669
jl777 <jameslee777@yahoo.com>
2018-04-30 13:49:04 +0300

    Test

--- iguana/exchanges/LP_rpc.c
+++ iguana/exchanges/LP_rpc.c
@@ -53,7 +53,7 @@ cJSON *bitcoin_json(struct iguana_info *coin,char *method,char *params)
         //    printf("issue.(%s, %s, %s, %s, %s)\n",coin->symbol,coin->serverport,coin->userpass,method,params);
         if ( coin->electrum != 0 && (strcmp(method,"getblock") == 0 || strcmp(method,"paxprice") == 0 || strcmp(method,"getrawmempool") == 0) )
             return(cJSON_Parse("{\"error\":\"illegal electrum call\"}"));
-        //if ( coin->inactive == 0 && (strcmp(method,"importprivkey") != 0  && strcmp(method,"validateaddress") != 0 && strcmp(method,"getaddressinfo") != 0 &&  strcmp(method,"importaddress") != 0 && strcmp(method,"getrawtransaction") != 0 && strcmp(method,"getblock") != 0 && strcmp(method,"getinfo") != 0 && strcmp(method,"getblockchaininfo") == 0) )
+        //if ( coin->inactive == 0 && (strcmp(method,"importprivkey") != 0  && strcmp(method,"validateaddress") != 0 && strcmp(method,"getaddressinfo") != 0 &&  strcmp(method,"importaddress") != 0 && strcmp(method,"getrawtransaction") != 0 && strcmp(method,"getblock") != 0 && strcmp(method,"getinfo") != 0 && strcmp(method,"getblockchaininfo") != 0) )
         {
             if ( coin->electrum == 0 )
             {
@@ -344,6 +344,8 @@ cJSON *LP_validateaddress(char *symbol,char *address)
     else
     {
         sprintf(buf,"[\"%s\"]",address);
+        if ( coin->validateaddress[0] == 0 )
+            strcpy(coin->validateaddress,"validateaddress");
         if ( (retjson= bitcoin_json(coin,coin->validateaddress,buf)) != 0 )
         {
             if ( jobj(retjson,"error") == 0 && jobj(retjson,"ismine") == 0 && strcmp(coin->validateaddress,"validateaddress") == 0 )
