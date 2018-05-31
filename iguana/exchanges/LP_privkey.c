562ee648ef2264f79d52500199023837b67117c3
jl777 <jameslee777@yahoo.com>
2018-05-30 23:45:35 -1100

    -print

--- iguana/exchanges/LP_privkey.c
+++ iguana/exchanges/LP_privkey.c
@@ -500,7 +500,7 @@ void verusblocks(struct iguana_info *coin,char *coinaddr)
                 if ( posflag != 0 )
                 {
                     numpos++;
-                    if ( num < 1000 )
+                    if ( num < 100 )
                         printf("ht.%-5d lock.%-7d PoS coinbase.(%s) staked.(%s) %.8f\n",height,locked,addr0,stakingaddr,value);
                     if ( strcmp(coinaddr,stakingaddr) == 0 )
                         possum += value, npos++;
@@ -510,7 +510,7 @@ void verusblocks(struct iguana_info *coin,char *coinaddr)
                 else
                 {
                     numpow++;
-                    if ( num < 1000 )
+                    if ( num < 100 )
                         printf("ht.%-5d lock.%-7d PoW coinbase.(%s) %.8f\n",height,locked,addr0,value);
                     if ( strcmp(coinaddr,addr0) == 0 || strcmp("RTu3JZZKLJTcfNwBa19dWRagEfQq49STqC",stakingaddr) == 0 )
                         powsum += value, npow++;
