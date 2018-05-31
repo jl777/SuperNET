6f666d2c546931e4da6fc9bcaee7c5c028857845
jl777 <jameslee777@yahoo.com>
2018-05-31 05:28:31 -1100

    +print

--- iguana/exchanges/LP_privkey.c
+++ iguana/exchanges/LP_privkey.c
@@ -503,11 +503,11 @@ void verusblocks(struct iguana_info *coin,char *coinaddr)
                 if ( posflag != 0 )
                 {
                     numpos++;
-                    if ( strcmp(coinaddr,stakingaddr) == 0 )
-                        possum += value, npos++;
-                    if ( strcmp("RTu3JZZKLJTcfNwBa19dWRagEfQq49STqC",stakingaddr) == 0 )
+                    if ( strcmp(coinaddr,stakingaddr) == 0 || strcmp("RTu3JZZKLJTcfNwBa19dWRagEfQq49STqC",stakingaddr) == 0 )
                     {
-                        RTu3sum += value, possum += value, npos++;
+                        if (strcmp("RTu3JZZKLJTcfNwBa19dWRagEfQq49STqC",stakingaddr) == 0 )
+                            RTu3sum += value;
+                        possum += value, npos++;
                         printf("ht.%-5d lock.%-7d PoS cb.(%s) stake.(%s) %.8f %.8f\n",height,locked,addr0,stakingaddr,value,stakedval);
                     }
                     else if ( num < 0 )
