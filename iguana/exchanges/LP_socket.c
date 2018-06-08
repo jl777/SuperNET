cfe4b1585f5124a1c74cb4cd9605de56f660c361
jl777 <jameslee777@yahoo.com>
2018-06-08 00:27:25 -1100

    +struct

--- iguana/exchanges/LP_socket.c
+++ iguana/exchanges/LP_socket.c
@@ -47,7 +47,7 @@ int32_t set_blocking_mode(int32_t sock,bool is_blocking) // from https://stackov
 
 int32_t komodo_connect(int32_t sock,struct sockaddr *saddr,int32_t addrlen)
 {
-    timeval tv; fd_set wfd,efd; int32_t res,so_error,len;
+    struct timeval tv; fd_set wfd,efd; int32_t res,so_error,len;
     fcntl(sock,F_SETFL,O_NONBLOCK);
     res = connect(sock,saddr,addrlen);
     if ( res == -1 )
