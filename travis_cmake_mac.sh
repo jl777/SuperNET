c310e6708c3095132e10ed945fa8a426df659326
Artem Pikulin <ortgma@gmail.com>
2018-07-05 14:07:27 +0700

    Try to update cmake on Mac travis.

--- travis_cmake_mac.sh
+++ travis_cmake_mac.sh
@@ -3,6 +3,7 @@ brew uninstall cmake --force
 wget https://cmake.org/files/v3.12/cmake-3.12.0-rc2-Darwin-x86_64.dmg
 hdiutil convert -quiet cmake-3.12.0-rc2-Darwin-x86_64.dmg -format UDTO -o bar
 hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint right_here bar.cdr
+ls right_here
 sudo installer -package right_here/cmake-3.12.0-rc2-Darwin-x86_64/cmake-3.12.0-rc2-Darwin-x86_64.pkg -target /
 sysctl -n hw.physicalcpu
 which cmake
