cbc1e68f7f2093529fdff24033a3e2c6b038dda3
Artem <artemciy@gmail.com>
2018-08-07 20:37:55 +0300

    Try to simplify the build to match a successfull local one

--- marketmaker_build_etomic.cmd
+++ marketmaker_build_etomic.cmd
@@ -1,51 +1,24 @@
 @echo off
 rem (c) Decker
 
+echo Windows version and architecture:
+ver
+wmic os get osarchitecture
+
 echo [#1] Install Rust, build nanomsg, curl and pthreads ...
 call marketmaker_build_depends.cmd
-copy marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\libcurl_imp.lib marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\curl.lib
-copy marketmaker_depends\pthread-win32\bin\x64_MSVC2015.Release\pthread_lib.lib marketmaker_depends\pthread-win32\bin\x64_MSVC2015.Release\pthread.lib
 
-echo [#2] Prepare build
-mkdir build_win64_release
-cd build_win64_release
-cmake .. -G "Visual Studio 14 2015 Win64" -DMM_VERSION="%APPVEYOR_BUILD_VERSION%"
+echo [#2] Prepare the build.
+mkdir build
+cd build
+cmake .. -DMM_VERSION="%APPVEYOR_BUILD_VERSION%"
 
-rem Steps before build:
-rem 
-rem crypto777\CMakeLists.txt 
-rem Add:
-rem if(WIN32)
-rem add_definitions(-DNATIVE_WINDOWS)
-rem add_definitions(-DIGUANA_LOG2PACKETSIZE=20)
-rem add_definitions(-DIGUANA_MAXPACKETSIZE=1572864)
-rem include_directories("${CMAKE_SOURCE_DIR}/includes")
-rem endif()
-rem
-rem iguana\exchanges\CMakeLists.txt 
-rem
-rem if(WIN32)
-rem add_definitions(-DNATIVE_WINDOWS)
-rem add_definitions(-DIGUANA_LOG2PACKETSIZE=20)
-rem add_definitions(-DIGUANA_MAXPACKETSIZE=1572864)
-rem add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-rem include_directories("${CMAKE_SOURCE_DIR}/includes")
-rem endif()
-rem
-rem iguana\exchanges\etomicswap\CMakeLists.txt 
-rem
-rem if(WIN32)
-rem add_definitions(-DNATIVE_WINDOWS)
-rem add_definitions(-DIGUANA_LOG2PACKETSIZE=20)
-rem add_definitions(-DIGUANA_MAXPACKETSIZE=1572864)
-rem add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-rem add_definitions(-DNOMINMAX)
-rem include_directories("${CMAKE_SOURCE_DIR}/includes")
-rem endif()
+echo [#3] Build the marketmaker-mainnet library.
 
-echo [#3] Build marketmaker-mainnet ...
-
-cmake --build . --config Release --target marketmaker-mainnet-lib
+cmake --build . --target marketmaker-mainnet-lib
 cd ..
+
+echo [#4] Build and test the MM2.
+
 cargo test
 cargo build
