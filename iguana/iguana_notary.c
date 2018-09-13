0159f5e602d36a33e03cc43d13d488440c02b480
blackjok3r <blackjok3r@gmail.com>
2018-09-13 13:58:30 +0800

    and another one

--- iguana/iguana_notary.c
+++ iguana/iguana_notary.c
@@ -67,7 +67,7 @@ void dpow_srcupdate(struct supernet_info *myinfo,struct dpow_info *dp,int32_t he
     dpow_fifoupdate(myinfo,dp->srcfifo,dp->last);
     if ( strcmp(dp->dest,"KMD") == 0 )
     {
-        checkpointfreq = DPOW_CHECKPOINTFREQ
+        checkpointfreq = DPOW_CHECKPOINTFREQ;
         if ( (strlen(dp->symbol) >= 4 && strncmp(dp->symbol, "STKD", 4) == 0) || (strlen(dp->symbol) >= 6 && strncmp(dp->symbol, "STAKED", 6) == 0) )
             checkpointfreq = 5;
         if ( dp->DESTHEIGHT < dp->prevDESTHEIGHT+checkpointfreq )
