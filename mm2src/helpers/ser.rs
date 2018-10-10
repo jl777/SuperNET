90bba12f2f193f66312fc7267995a73735385e23
Artem <artemciy@gmail.com>
2018-10-11 02:25:34 +0300

    Fix imports for de_none_if_empty

--- mm2src/helpers/ser.rs
+++ mm2src/helpers/ser.rs
@@ -1,4 +1,4 @@
-use serde::de::Deserializer;
+use serde::de::{self, Deserializer};
 use std::fmt;
 
 /// Deserializes an empty string into `None`.  
@@ -6,12 +6,12 @@ use std::fmt;
 /// Use with `#[serde(default, deserialize_with = "de_none_if_empty")]`.
 pub fn de_none_if_empty<'de, D: Deserializer<'de>> (des: D) -> Result<Option<String>, D::Error> {
     struct Visitor;
-    impl<'de> serde::de::Visitor<'de> for Visitor {
+    impl<'de> de::Visitor<'de> for Visitor {
         type Value = Option<String>;
         fn expecting (&self, fm: &mut fmt::Formatter) -> fmt::Result {fm.write_str ("Optional string")}
-        fn visit_none<E> (self) -> Result<Option<String>, E> where E: serde::de::Error {Ok (None)}
-        fn visit_unit<E> (self) -> Result<Option<String>, E> where E: serde::de::Error {Ok (None)}
-        fn visit_str<E> (self, sv: &str) -> Result<Option<String>, E> where E: serde::de::Error {
+        fn visit_none<E> (self) -> Result<Option<String>, E> where E: de::Error {Ok (None)}
+        fn visit_unit<E> (self) -> Result<Option<String>, E> where E: de::Error {Ok (None)}
+        fn visit_str<E> (self, sv: &str) -> Result<Option<String>, E> where E: de::Error {
             if sv.is_empty() {
                 Ok (None)
             } else {
