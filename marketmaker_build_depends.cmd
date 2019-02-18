f917fc7ff2d4f4b79877aae3b84f86dd7ec98711
Artem Pikulin <ortgma@gmail.com>
2019-02-18 22:49:41 +0700

    Move Sodium win build to separate script. Etomic uses precompiled lib.

--- marketmaker_build_depends.cmd
+++ marketmaker_build_depends.cmd
@@ -99,16 +99,3 @@ if not exist marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\libcurl_
 copy marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\libcurl_imp.lib marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\curl.lib
 copy marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\libcurl_imp.exp marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\curl.exp
 copy marketmaker_depends\curl\build_msvc_2015_win64\lib\Release\libcurl.dll x64\Release\libcurl.dll
-
-rem --- libsodium ---
-:compile_libsodium
-cd marketmaker_depends 
-git clone https://github.com/jedisct1/libsodium
-cd libsodium\builds\msvc\vs2015
-MSBuild libsodium.sln /t:Rebuild /p:Configuration=DynRelease /p:Platform=Win32
-MSBuild libsodium.sln /t:Rebuild /p:Configuration=DynRelease /p:Platform=x64
-cd ..\..\..\..\..
-xcopy marketmaker_depends\libsodium\src\libsodium\include includes /O /X /E /H /K /Y
-xcopy marketmaker_depends\libsodium\bin\x64\Release\v140\dynamic OSlibs\win\x64\release /O /X /E /H /K /Y
-xcopy marketmaker_depends\libsodium\bin\Win32\Release\v140\dynamic OSlibs\win /O /X /E /H /K /Y
-copy marketmaker_depends\libsodium\bin\x64\Release\v140\dynamic\libsodium.dll x64\Release 
