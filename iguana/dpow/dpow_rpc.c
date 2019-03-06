af6998ef1127cd4057dd8efdda5128d2ea54401d
blackjok3r <blackjok3r@gmail.com>
2019-03-07 02:19:31 +0800

    kmd-5 for safety.

--- iguana/dpow/dpow_rpc.c
+++ iguana/dpow/dpow_rpc.c
@@ -317,7 +317,8 @@ int32_t dpow_paxpending(struct supernet_info *myinfo,uint8_t *hex,int32_t hexsiz
                 kmdheight = jint(infojson,"blocks");
                 free_json(infojson);
             }
-            if ( (retjson= dpow_MoMoMdata(kmdcoin,bp->srccoin->symbol,kmdheight,bp->CCid)) != 0 )
+            // 5 is easily enough.. KMD is pretty safe 
+            if ( (retjson= dpow_MoMoMdata(kmdcoin,bp->srccoin->symbol,kmdheight-5,bp->CCid)) != 0 )
             {
                 if ( ppMoMheight != 0 && jstr(retjson,"error") != 0 )
                 {
