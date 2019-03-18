ed4725b92511816d6a99df9525d0c2968c0530c5
Artem Pikulin <ortgma@gmail.com>
2019-03-18 19:25:05 +0700

    #321 Reject "dust" UTXO outputs (< fee). Add change "dust" to miner fee. Add coins crate to workspace in order to test it on every CI build using --all.

--- mm2src/coins/utxo.rs
+++ mm2src/coins/utxo.rs
@@ -466,7 +466,9 @@ impl UtxoCoin {
 
             let mut target_value = 0;
             for output in outputs.iter() {
-                target_value += output.value;
+                let value = output.value;
+                true_or_err!(value >= tx_fee, "Output value {} is less than tx_fee {}", value, tx_fee);
+                target_value += value;
             }
 
             true_or_err!(target_value > 0, "Total target value calculated from outputs {:?} is zero", outputs);
@@ -486,10 +488,11 @@ impl UtxoCoin {
 
             true_or_err!(value_to_spend >= target_value, "Not sufficient balance. Couldn't collect enough value from utxos {:?} to create tx with outputs {:?}", utxos, outputs);
 
-            if value_to_spend > target_value {
+            let change = value_to_spend - target_value;
+            if change >= tx_fee {
                 outputs.push({
                     TransactionOutput {
-                        value: value_to_spend - target_value,
+                        value: change,
                         script_pubkey: change_script_pubkey
                     }
                 });
@@ -1057,16 +1060,86 @@ pub fn utxo_coin_from_iguana_info(info: *mut lp::iguana_info, mode: UtxoInitMode
     Ok(UtxoCoin(Arc::new(coin)).into())
 }
 
-#[test]
-fn test_extract_secret() {
-    let bytes = hex::decode("0100000001de7aa8d29524906b2b54ee2e0281f3607f75662cbc9080df81d1047b78e21dbc00000000d7473044022079b6c50820040b1fbbe9251ced32ab334d33830f6f8d0bf0a40c7f1336b67d5b0220142ccf723ddabb34e542ed65c395abc1fbf5b6c3e730396f15d25c49b668a1a401209da937e5609680cb30bff4a7661364ca1d1851c2506fa80c443f00a3d3bf7365004c6b6304f62b0e5cb175210270e75970bb20029b3879ec76c4acd320a8d0589e003636264d01a7d566504bfbac6782012088a9142fb610d856c19fd57f2d0cffe8dff689074b3d8a882103f368228456c940ac113e53dad5c104cf209f2f102a409207269383b6ab9b03deac68ffffffff01d0dc9800000000001976a9146d9d2b554d768232320587df75c4338ecc8bf37d88ac40280e5c").unwrap();
-    let tx: UtxoTx = deserialize(bytes.as_slice()).unwrap();
-    let extended = UtxoTx {
-        transaction: tx,
-        redeem_script: vec![].into()
-    };
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let secret = extended.extract_secret().unwrap();
-    let expected_secret = hex::decode("9da937e5609680cb30bff4a7661364ca1d1851c2506fa80c443f00a3d3bf7365").unwrap();
-    assert_eq!(expected_secret, secret);
+    fn utxo_coin_for_test() -> UtxoCoin {
+        let key_pair = key_pair_from_seed("test seed".as_bytes(), 0);
+        let my_address = Address {
+            prefix: 60,
+            hash: key_pair.public().address_hash(),
+            t_addr_prefix: 0
+        };
+
+        let client = ElectrumClientImpl::new();
+        log!("My address "(my_address));
+
+        let coin = UtxoCoinImpl {
+            decimals: 8,
+            rpc_client: UtxoRpcClientEnum::Electrum(ElectrumClient(Arc::new(client))),
+            key_pair,
+            is_pos: false,
+            notarized: false,
+            overwintered: true,
+            rpc_password: "".to_owned(),
+            rpc_port: 0,
+            rpc_user: "".to_owned(),
+            segwit: false,
+            tx_version: 4,
+            my_address,
+            asset_chain: true,
+            p2sh_addr_prefix: 85,
+            p2sh_t_addr_prefix: 0,
+            pub_addr_prefix: 60,
+            pub_t_addr_prefix: 0,
+            ticker: "ETOMIC".into(),
+            wif_prefix: 0,
+            tx_fee: TxFee::Fixed(1000),
+            version_group_id: 0x892f2085
+        };
+
+        UtxoCoin(Arc::new(coin))
+    }
+
+    #[test]
+    fn test_extract_secret() {
+        let tx: UtxoTx = "0100000001de7aa8d29524906b2b54ee2e0281f3607f75662cbc9080df81d1047b78e21dbc00000000d7473044022079b6c50820040b1fbbe9251ced32ab334d33830f6f8d0bf0a40c7f1336b67d5b0220142ccf723ddabb34e542ed65c395abc1fbf5b6c3e730396f15d25c49b668a1a401209da937e5609680cb30bff4a7661364ca1d1851c2506fa80c443f00a3d3bf7365004c6b6304f62b0e5cb175210270e75970bb20029b3879ec76c4acd320a8d0589e003636264d01a7d566504bfbac6782012088a9142fb610d856c19fd57f2d0cffe8dff689074b3d8a882103f368228456c940ac113e53dad5c104cf209f2f102a409207269383b6ab9b03deac68ffffffff01d0dc9800000000001976a9146d9d2b554d768232320587df75c4338ecc8bf37d88ac40280e5c".into();
+        let secret = tx.extract_secret().unwrap();
+        let expected_secret = hex::decode("9da937e5609680cb30bff4a7661364ca1d1851c2506fa80c443f00a3d3bf7365").unwrap();
+        assert_eq!(expected_secret, secret);
+    }
+
+    #[test]
+    fn test_generate_transaction() {
+        let coin = utxo_coin_for_test();
+        let unspents = vec![UnspentInfo {
+            value: 10000000000,
+            outpoint: OutPoint::default(),
+        }];
+
+        let outputs = vec![TransactionOutput {
+            script_pubkey: vec![].into(),
+            value: 999,
+        }];
+
+        let generated = coin.generate_transaction(unspents, outputs, 0).wait();
+        // must not allow to use output with value < tx_fee
+        unwrap_err!(generated);
+
+        let unspents = vec![UnspentInfo {
+            value: 100000,
+            outpoint: OutPoint::default(),
+        }];
+
+        let outputs = vec![TransactionOutput {
+            script_pubkey: vec![].into(),
+            value: 98001,
+        }];
+
+        let generated = unwrap!(coin.generate_transaction(unspents, outputs, 0).wait());
+        // the change that is less than tx_fee must be included to miner fee according to JL777
+        // so no extra outputs should appear in generated transaction
+        assert_eq!(generated.0.outputs.len(), 1);
+    }
 }
