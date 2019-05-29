da2539ad9ac934b6ced466b2c9a619ebdb53b7c2
Artemciy <artemciy@gmail.com>
2019-05-29 19:16:11 +0300

    Catch and log test panics

--- mm2src/mm2_lib.rs
+++ mm2src/mm2_lib.rs
@@ -28,20 +28,30 @@ pub extern fn mm2_test (torch: i32, log_cb: extern fn (line: *const c_char)) -> 
         panic! ("Can't lock LOG_OUTPUT")
     }
 
-    log! ("test_status…");
-    common::log::tests::test_status();
+    // NB: We have to catch the panic because the error isn't logged otherwise.
+    // (In the release mode the `ud2` op will trigger a crash or debugger on panic
+    // but we don't have debugging symbols in the Rust code then).
+    let rc = catch_unwind (|| {
+        log! ("mm2_test] test_status…");
+        common::log::tests::test_status();
 
-    log! ("peers_dht…");
-    peers::peers_tests::peers_dht();
+        log! ("mm2_test] peers_dht…");
+        peers::peers_tests::peers_dht();
 
-    log! ("peers_direct_send…");
-    peers::peers_tests::peers_direct_send();
+        log! ("mm2_test] peers_direct_send…");
+        peers::peers_tests::peers_direct_send();
 
-    log! ("peers_http_fallback_kv…");
-    peers::peers_tests::peers_http_fallback_kv();
+        log! ("mm2_test] peers_http_fallback_kv…");
+        peers::peers_tests::peers_http_fallback_kv();
 
-    log! ("peers_http_fallback_recv…");
-    peers::peers_tests::peers_http_fallback_recv();
-
-    torch
+        log! ("mm2_test] peers_http_fallback_recv…");
+        peers::peers_tests::peers_http_fallback_recv();
+    });
+    if let Err (err) = rc {
+        log! ("mm2_test] There was an error: " (any_to_str (&*err) .unwrap_or ("-")));
+        -1
+    } else {
+        log! ("mm2_test] All done, passing the torch.");
+        torch
+    }
 }
