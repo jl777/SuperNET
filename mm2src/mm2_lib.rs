fc953325008b60629b58b7cf43174c653983ce01
Artemciy <artemciy@gmail.com>
2019-06-10 13:33:05 +0300

    Cherry pick mm2_test from dc201cff

--- mm2src/mm2_lib.rs
+++ mm2src/mm2_lib.rs
@@ -19,3 +19,29 @@ mod mm2;
 pub extern fn mm2_main() {
     mm2::mm2_main()
 }
+
+#[no_mangle]
+pub extern fn mm2_test (torch: i32, log_cb: extern fn (line: *const c_char)) -> i32 {
+    if let Ok (mut log_output) = LOG_OUTPUT.lock() {
+        *log_output = Some (log_cb);
+    } else {
+        panic! ("Can't lock LOG_OUTPUT")
+    }
+
+    log! ("test_status…");
+    common::log::tests::test_status();
+
+    log! ("peers_dht…");
+    peers::peers_tests::peers_dht();
+
+    log! ("peers_direct_send…");
+    peers::peers_tests::peers_direct_send();
+
+    log! ("peers_http_fallback_kv…");
+    peers::peers_tests::peers_http_fallback_kv();
+
+    log! ("peers_http_fallback_recv…");
+    peers::peers_tests::peers_http_fallback_recv();
+
+    torch
+}
