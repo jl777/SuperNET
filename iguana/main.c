236ab3d3a0e3b15258fc2cb35684d24c1e224ea7
blackjok3r <blackjok3r@gmail.com>
2019-06-24 15:18:42 +0800

    initial commit for dual iguanas

--- iguana/main.c
+++ iguana/main.c
@@ -2250,17 +2250,15 @@ void iguana_main(void *arg)
         }
         else if ( strncmp((char *)arg,"notary",strlen("notary")) == 0 ) // must be second to last
         {
-            if ( strcmp((char *)arg,"notary_thirdparty") == 0 )
-                myinfo->rpcport = IGUANA_NOTARYPORT2;
-            else 
-                myinfo->rpcport = IGUANA_NOTARYPORT;
+            myinfo->rpcport = IGUANA_NOTARYPORT;
             myinfo->nosplit = 1;
             myinfo->IAMNOTARY = 1;
             myinfo->DEXEXPLORER = 0;//1; disable as SPV is used now
         }
         else
         {
-            myinfo->rpcport = IGUANA_NOTARYPORT;
+            // this means that an elected file was specified for 3rd party network, so use diffrent RPC port. 
+            myinfo->rpcport = IGUANA_NOTARYPORT2;
             myinfo->IAMNOTARY = 1;
             myinfo->DEXEXPLORER = 0;//1; disable as SPV is used now
             elected = (char *)arg;
