ee7510517fbfcd8b619c736c762e1fea76c07f3b
jl777 <jameslee777@yahoo.com>
2019-06-26 19:19:24 -1100

    Conversion guard

--- iguana/exchanges/LP_commands.c
+++ iguana/exchanges/LP_commands.c
@@ -417,12 +417,12 @@ version\n\
                 jaddbits256(retjson,"privkey",privkey);
                 bitcoin_priv2wif(coin,wiftaddr,wifstr,privkey,wiftype);
                 jaddstr(retjson,"wif",wifstr);
-                if ( passphrase[0] == 'U' )
+                bitcoin_wif2priv("KMD",188,&tmptype,&privkey,passphrase);
+                if ( bits256_nonz(privkey) != 0 )
                 {
                     uint8_t tmptype; bits256 pubkey;
                     privkey = LP_privkeycalc(ctx,pubkey33,&pubkey,ptr,passphrase,"");
 
-                    //bitcoin_wif2priv("KMD",188,&tmptype,&privkey,passphrase);
                     bitcoin_priv2wif(coin,wiftaddr,wifstr,privkey,wiftype);
                     jaddstr(retjson,"convwif",wifstr);
                     bitcoin_priv2pub(ctx,coin,pubkey33,coinaddr,privkey,taddr,pubtype);
