719b342c1ca5d230b9e9b3deb26f9796d51bf21b
blackjok3r <blackjok3r@gmail.com>
2019-07-29 08:03:11 +0800

    try

--- iguana/dpow/dpow_network.c
+++ iguana/dpow/dpow_network.c
@@ -2000,20 +2000,18 @@ void dpow_notarize_update(struct supernet_info *myinfo,struct dpow_info *dp,stru
             bp->bestmask = dpow_maskmin(bp->recvmask,bp,&bp->bestk);
         }
         
-
         // check that block has advanced by 1 on KMD before allowing bestmask to be calculated 
         if ( strcmp(bp->destcoin->symbol,"KMD") == 0 )
         {
             if ( bp->destht_start == bp->destcoin->longestchain )
                 return;
-            else fprintf(stderr, "%s->KMD: checkpoint ht.%i vs longestchain.%i\n",bp->srccoin->symbol, bp->destht_start, bp->destcoin->longestchain);
         }
         else 
         {
             if ( bp->height == bp->srccoin->longestchain )
                 return;
-            else fprintf(stderr, "KMD->BTC: checkpoint ht.%i vs longestchain.%i\n", bp->height , bp->srccoin->longestchain);
         }  
+        fprintf(stderr, "[%s] checkpoint ht.%i vs longestchain.%i\n",bp->srccoin->symbol, bp->destht_start, bp->destcoin->longestchain);
         
         dpow_bestconsensus(dp,bp);
         if ( bp->bestk >= 0 )
